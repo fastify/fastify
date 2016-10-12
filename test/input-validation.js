5c1e2020ee1d02f2d365c88a20e4afcaedeb7340
delvedor <tommydelved@gmail.com>
2016-10-12 09:44:07 +0200

    Updated test

---
+++ test/input-validation.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const request = require('request')
+const http = require('http')
+
+module.exports.payloadMethod = function (method, t) {
+  const test = t.test
+  const fastify = require('..')()
+  const server = http.createServer(fastify)
+  const upMethod = method.toUpperCase()
+  const loMethod = method.toLowerCase()
+
+  const schema = {
+    in: {
+      type: 'object',
+      properties: {
+        hello: {
+          type: 'integer'
+        }
+      }
+    }
+  }
+
+  test(`${upMethod} can be created`, t => {
+    t.plan(1)
+    try {
+      fastify[loMethod]('/', schema, function (req, reply) {
+        reply(null, 200, req.body)
+      })
+      t.pass()
+    } catch (e) {
+      t.fail()
+    }
+  })
+
+  server.listen(0, function (err) {
+    if (err) {
+      t.error(err)
+    }
+
+    server.unref()
+
+    test(`${upMethod} - correctly replies`, t => {
+      t.plan(3)
+      request({
+        method: upMethod,
+        uri: 'http://localhost:' + server.address().port,
+        body: {
+          hello: 42
+        },
+        json: true
+      }, (err, response, body) => {
+        t.error(err)
+        t.strictEqual(response.statusCode, 200)
+        t.deepEqual(body, { hello: 42 })
+      })
+    })
+
+    test(`${upMethod} - 400 on bad parameters`, t => {
+      t.plan(3)
+      request({
+        method: upMethod,
+        uri: 'http://localhost:' + server.address().port,
+        body: {
+          hello: 'world'
+        },
+        json: true
+      }, (err, response, body) => {
+        t.error(err)
+        t.strictEqual(response.statusCode, 400)
+        t.deepEqual(body[0], {
+          keyword: 'type',
+          dataPath: '.hello',
+          schemaPath: '#/properties/hello/type',
+          params: { type: 'integer' },
+          message: 'should be integer'
+        })
+      })
+    })
+  })
+}
--- test/post.test.js
+++ test/post.test.js
@@ -1,3 +1,5 @@
 'use strict'
 
-require('./helper').payloadMethod('post', require('tap'))
+const t = require('tap')
+require('./helper').payloadMethod('post', t)
+require('./input-validation').payloadMethod('post', t)

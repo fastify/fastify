09d198757de5d79311996cab175cf8a411115466
delvedor <tommydelved@gmail.com>
2016-10-14 09:25:00 +0200

    Updated test

--- test/get.test.js
+++ test/get.test.js
@@ -18,11 +18,34 @@ const schema = {
   }
 }
 
+const querySchema = {
+  querystring: {
+    type: 'object',
+    properties: {
+      hello: {
+        type: 'integer'
+      }
+    }
+  }
+}
+
 test('shorthand - get', t => {
   t.plan(1)
   try {
     fastify.get('/', schema, function (req, reply) {
       reply(null, 200, { hello: 'world' })
+    })
+    t.pass()
+  } catch (e) {
+    t.fail()
+  }
+})
+
+test('shorthand - get, querystring schema', t => {
+  t.plan(1)
+  try {
+    fastify.get('/query', querySchema, function (req, reply) {
+      reply(null, 200, req.query)
     })
     t.pass()
   } catch (e) {
@@ -56,6 +79,37 @@ server.listen(0, err => {
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-length'], '' + body.length)
       t.deepEqual(JSON.parse(body), { hello: 'world' })
+    })
+  })
+
+  test('shorthand - request get querystring schema', t => {
+    t.plan(4)
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + server.address().port + '/query?hello=123'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.deepEqual(JSON.parse(body), { hello: 123 })
+    })
+  })
+
+  test('shorthand - request get querystring schema error', t => {
+    t.plan(3)
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + server.address().port + '/query?hello=world'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 400)
+      t.deepEqual(JSON.parse(body)[0], {
+        keyword: 'type',
+        dataPath: '.hello',
+        schemaPath: '#/properties/hello/type',
+        params: { type: 'integer' },
+        message: 'should be integer'
+      })
     })
   })
 

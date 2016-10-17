dd45e0e5566d197a57fd2d2f3a805bd5e8dd2314
delvedor <tommydelved@gmail.com>
2016-10-17 12:24:40 +0200

    Plugin async load

--- fastify.js
+++ fastify.js
@@ -4,6 +4,7 @@ const wayfarer = require('wayfarer')
 const urlUtil = require('url')
 const stripUrl = require('pathname-match')
 const jsonParser = require('body/json')
+const bootInTheArse = require('boot-in-the-arse')
 
 const supportedMethods = ['GET', 'POST', 'PUT']
 const validation = require('./lib/validation')
@@ -14,6 +15,7 @@ const serialize = validation.serialize
 function build () {
   const router = wayfarer('/404')
   const map = new Map()
+  bootInTheArse(fastify)
   router.on('/404', defaultRoute)
 
   // shorthand methods
@@ -22,6 +24,8 @@ function build () {
   fastify.put = put
   // extended route
   fastify.route = route
+  // plugin
+  // fastify.use = boot.use
 
   return fastify
 

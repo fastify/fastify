557aa381378c4c45c748f08ee70cf27e44babe25
delvedor <tommydelved@gmail.com>
2016-10-26 09:30:54 +0200

    Moved tier-node logic to lib

--- fastify.js
+++ fastify.js
@@ -1,16 +1,12 @@
 'use strict'
 
 const wayfarer = require('wayfarer')
-const urlUtil = require('url')
 const stripUrl = require('pathname-match')
-const jsonParser = require('body/json')
 const pluginLoader = require('boot-in-the-arse')
 
-const supportedMethods = ['DELETE', 'GET', 'HEAD', 'PATCH', 'POST', 'PUT']
-const validation = require('./lib/validation')
-const buildSchema = validation.buildSchema
-const validateSchema = validation.validateSchema
-const serialize = validation.serialize
+const supportedMethods = ['DELETE', 'GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'OPTIONS']
+const buildSchema = require('./lib/validation').build
+const buildNode = require('./lib/tier-node')
 
 function build () {
   const router = wayfarer('/404')
@@ -19,12 +15,13 @@ function build () {
   router.on('/404', defaultRoute)
 
   // shorthand methods
-  fastify.delete = delete_
-  fastify.get = get
-  fastify.head = head
-  fastify.patch = patch
-  fastify.post = post
-  fastify.put = put
+  fastify.delete = _delete
+  fastify.get = _get
+  fastify.head = _head
+  fastify.patch = _patch
+  fastify.post = _post
+  fastify.put = _put
+  fastify.options = _options
   // extended route
   fastify.route = route
   // plugin
@@ -36,28 +33,33 @@ function build () {
     router(stripUrl(req.url), req, res)
   }
 
-  function delete_ (url, schema, handler) {
+  // Shorthand methods
+  function _delete (url, schema, handler) {
     return _route('DELETE', url, schema, handler)
   }
 
-  function get (url, schema, handler) {
+  function _get (url, schema, handler) {
     return _route('GET', url, schema, handler)
   }
 
-  function head (url, schema, handler) {
+  function _head (url, schema, handler) {
     return _route('HEAD', url, schema, handler)
   }
 
-  function patch (url, schema, handler) {
+  function _patch (url, schema, handler) {
     return _route('PATCH', url, schema, handler)
   }
 
-  function post (url, schema, handler) {
+  function _post (url, schema, handler) {
     return _route('POST', url, schema, handler)
   }
 
-  function put (url, schema, handler) {
+  function _put (url, schema, handler) {
     return _route('PUT', url, schema, handler)
+  }
+
+  function _options (url, schema, handler) {
+    return _route('OPTIONS', url, schema, handler)
   }
 
   function _route (method, url, schema, handler) {
@@ -68,6 +70,7 @@ function build () {
     return route({ method, url, schema, handler })
   }
 
+  // Route management
   function route (opts) {
     if (supportedMethods.indexOf(opts.method) === -1) {
       throw new Error(`${opts.method} method is not supported!`)
@@ -86,110 +89,17 @@ function build () {
 
       map.get(opts.url)[opts.method] = opts
     } else {
-      const node = createNode(opts.url)
+      const node = buildNode(opts.url, router)
       node[opts.method] = opts
       map.set(opts.url, node)
     }
+    // chainable api
     return fastify
-  }
-
-  function bodyParsed (handle, params, req, res) {
-    function parsed (err, body) {
-      if (err) {
-        res.statusCode = 422
-        res.end()
-        return
-      }
-      handleNode(handle, params, req, res, body, null)
-    }
-    return parsed
-  }
-
-  function createNode (url) {
-    const node = {}
-
-    router.on(url, function handle (params, req, res) {
-      const handle = node[req.method]
-
-      if (handle) {
-        switch (req.method) {
-          case 'GET':
-          case 'HEAD':
-          case 'DELETE':
-            handleNode(handle, params, req, res, null, urlUtil.parse(req.url, true).query)
-            break
-          case 'PATCH':
-            if (req.headers['content-type'] === 'application/json') {
-              jsonParser(req, bodyParsed(handle, params, req, res))
-            } else {
-              res.statusCode = 415
-              res.end()
-            }
-            break
-          case 'POST':
-          case 'PUT':
-            if (req.headers['content-type'] === 'application/json') {
-              jsonParser(req, bodyParsed(handle, params, req, res))
-            } else {
-              res.statusCode = 415
-              res.end()
-            }
-            break
-          default:
-            res.statusCode = 404
-            res.end()
-        }
-      } else {
-        res.statusCode = 404
-        res.end()
-      }
-    })
-
-    return node
-  }
-
-  function handleNode (handle, params, req, res, body, query) {
-    if (!handle) {
-      res.statusCode = 404
-      res.end()
-    }
-
-    const valid = validateSchema(handle, params, body, query)
-    if (valid !== true) {
-      res.statusCode = 400
-      res.end(valid)
-      return
-    }
-
-    const request = new Request(params, req, body, query)
-    handle.handler(request, reply)
-
-    function reply (err, statusCode, data) {
-      if (err) {
-        res.statusCode = 500
-        res.end()
-      }
-
-      if (!data) {
-        data = statusCode
-        statusCode = 200
-      }
-
-      res.statusCode = statusCode
-      res.end(serialize(handle, data))
-    }
   }
 
   function defaultRoute (params, req, res) {
     res.statusCode = 404
     res.end()
-  }
-
-  function Request (params, req, body, query) {
-    this.params = params
-    this.req = req
-    this.body = body
-    this.query = query
   }
 }
 

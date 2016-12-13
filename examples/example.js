9bfe6c69182d3f8a4edd2591b95b525e3dffdfb5
delvedor <tommydelved@gmail.com>
2016-12-13 12:52:27 +0100

    Update example for bench test

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fastify = require('../fastify')()
+const Promise = require('bluebird')
 
 const schema = {
   out: {
@@ -15,8 +16,15 @@ const schema = {
 
 fastify
   .get('/', schema, function (req, reply) {
-    reply.header('Content-Type', 'application/json').code(200)
-    reply.send({ hello: 'world' })
+    // reply.header('Content-Type', 'application/json').code(200)
+    // reply.send({ hello: 'world' })
+    reply.header('content-type', 'text/plain').code(200).send('callback!')
+  })
+  .get('/promise', function (req, reply) {
+    const promise = new Promise(function (resolve, reject) {
+      resolve('promise!')
+    })
+    reply.header('content-type', 'text/plain').code(200).send(promise)
   })
   .post('/', schema, function (req, reply) {
     reply.send(null, { hello: 'world' })

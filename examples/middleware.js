d5a5a723eb26a14fe0b6a1c922d7b8e4da3f0357
Matteo Collina <hello@matteocollina.com>
2017-02-09 16:54:47 +0100

    Added tests for cors and helmet compatibility.

---
+++ examples/middleware.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const fastify = require('../fastify')()
+const cors = require('cors')
+const helmet = require('cors')
+
+const schema = {
+  out: {
+    type: 'object',
+    properties: {
+      hello: {
+        type: 'string'
+      }
+    }
+  }
+}
+
+fastify
+  .use(cors())
+  .use(helmet())
+  .get('/', schema, function (req, reply) {
+    reply.header('Content-Type', 'application/json').code(200)
+    reply.send({ hello: 'world' })
+  })
+
+fastify.listen(3000, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})

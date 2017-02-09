da4d6d83b78c461f795ffe5788b772a364ee4148
delvedor <tommydelved@gmail.com>
2017-02-10 00:22:45 +0100

    Updated https test with rejectUnauthorize

--- test/https/https.test.js
+++ test/https/https.test.js
@@ -7,9 +7,6 @@ const fs = require('fs')
 const path = require('path')
 const Fastify = require('../..')
 
-// Because we are using a self signed certificate for testing
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
 try {
   var fastify = Fastify({
     https: {
@@ -42,7 +39,8 @@ fastify.listen(0, err => {
     t.plan(4)
     request({
       method: 'GET',
-      uri: 'https://localhost:' + fastify.server.address().port
+      uri: 'https://localhost:' + fastify.server.address().port,
+      rejectUnauthorized: false
     }, (err, response, body) => {
       t.error(err)
       t.strictEqual(response.statusCode, 200)

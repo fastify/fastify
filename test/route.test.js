4ae60670ce61a290b0ac6a4d582cb421324b653b
James Sumners <james.sumners@gmail.com>
2017-04-12 11:11:55 -0400

    Add `path` alias for `url` in route options

--- test/route.test.js
+++ test/route.test.js
@@ -75,3 +75,30 @@ fastify.listen(0, function (err) {
     })
   })
 })
+
+test('path can be specified in place of uri', t => {
+  t.plan(3)
+
+  fastify.listen(0, function (err) {
+    if (err) t.error(err)
+    fastify.server.unref()
+
+    fastify.route({
+      method: 'GET',
+      path: '/path',
+      handler: function (req, reply) {
+        reply.send({ hello: 'world' })
+      }
+    })
+
+    const reqOpts = {
+      method: 'GET',
+      uri: 'http://localhost:' + fastify.server.address().port + '/path'
+    }
+    request(reqOpts, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
+    })
+  })
+})

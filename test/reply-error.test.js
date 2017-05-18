9d3269f609aa498882d52f186ce170933e6e7293
Tommaso Allevi <tomallevi@gmail.com>
2017-05-18 20:06:19 +0200

    Split assertions in different tests

--- test/reply-error.test.js
+++ test/reply-error.test.js
@@ -2,7 +2,6 @@
 
 const t = require('tap')
 const test = t.test
-// const safeStringify = require('fast-safe-stringify')
 const Fastify = require('..')
 const statusCodes = require('../lib/status-codes.json')
 const boom = require('boom')
@@ -151,7 +150,7 @@ test('onRequest hook error handling with code inside done', t => {
 })
 
 test('support for boom', t => {
-  t.plan(7)
+  t.plan(3)
   const fastify = Fastify()
 
   fastify.get('/500', (req, reply) => {
@@ -166,56 +165,68 @@ test('support for boom', t => {
     reply.send(boom.unauthorized('invalid password', 'sample'))
   })
 
-  fastify.inject({
-    method: 'GET',
-    url: '/500'
-  }, res => {
-    t.strictEqual(res.statusCode, 500)
-    t.deepEqual(
-      // we are doing this because Boom creates also a stack that is stripped during the stringify phase
-      JSON.parse(res.payload),
-      {
-        statusCode: 500,
-        error: 'Internal Server Error',
-        message: 'An internal server error occurred'
-      }
-    )
-  })
+  t.test('500', t => {
+    t.plan(2)
 
-  fastify.inject({
-    method: 'GET',
-    url: '/400'
-  }, res => {
-    t.strictEqual(res.statusCode, 400)
-    t.deepEqual(
-      // we are doing this because Boom creates also a stack that is stripped during the stringify phase
-      JSON.parse(res.payload),
-      {
-        statusCode: 400,
-        error: 'Bad Request',
-        message: 'winter is coming'
-      }
-    )
-  })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/401'
-  }, res => {
-    t.strictEqual(res.statusCode, 401)
-    t.deepEqual(
-      // we are doing this because Boom creates also a stack that is stripped during the stringify phase
-      JSON.parse(res.payload),
-      {
-        statusCode: 401,
-        error: 'Unauthorized',
-        message: 'invalid password',
-        attributes: {
-          error: 'invalid password'
+    fastify.inject({
+      method: 'GET',
+      url: '/500'
+    }, res => {
+      t.strictEqual(res.statusCode, 500)
+      t.deepEqual(
+        // we are doing this because Boom creates also a stack that is stripped during the stringify phase
+        JSON.parse(res.payload),
+        {
+          statusCode: 500,
+          error: 'Internal Server Error',
+          message: 'An internal server error occurred'
         }
-      }
-    )
-    t.deepEqual(res.headers['www-authenticate'], 'sample error="invalid password"')
+      )
+    })
+  })
+
+  t.test('400', t => {
+    t.plan(2)
+
+    fastify.inject({
+      method: 'GET',
+      url: '/400'
+    }, res => {
+      t.strictEqual(res.statusCode, 400)
+      t.deepEqual(
+        // we are doing this because Boom creates also a stack that is stripped during the stringify phase
+        JSON.parse(res.payload),
+        {
+          statusCode: 400,
+          error: 'Bad Request',
+          message: 'winter is coming'
+        }
+      )
+    })
+  })
+
+  t.test('401', t => {
+    t.plan(3)
+
+    fastify.inject({
+      method: 'GET',
+      url: '/401'
+    }, res => {
+      t.strictEqual(res.statusCode, 401)
+      t.deepEqual(
+        // we are doing this because Boom creates also a stack that is stripped during the stringify phase
+        JSON.parse(res.payload),
+        {
+          statusCode: 401,
+          error: 'Unauthorized',
+          message: 'invalid password',
+          attributes: {
+            error: 'invalid password'
+          }
+        }
+      )
+      t.deepEqual(res.headers['www-authenticate'], 'sample error="invalid password"')
+    })
   })
 })
 

34dca3e1b0425a894e8d623c30c4e9cc063a0bc7
Matteo Collina <hello@matteocollina.com>
2017-06-01 11:33:36 +0200

    Updated deps and tests passing on node 8.

--- test/async-await.test.js
+++ test/async-await.test.js
@@ -4,7 +4,7 @@ const path = require('path')
 const childProcess = require('child_process')
 const tap = require('tap')
 
-if (Number(process.versions.node[0]) >= 7) {
+if (Number(process.versions.node[0]) === 7) {
   childProcess.fork(path.join(__dirname, 'async-await'), [], {
     execArgv: ['--harmony-async-await']
   })

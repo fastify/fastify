8468bf63b930343d66a84a1aaeb6155d988edeaa
delvedor <tommydelved@gmail.com>
2017-06-19 22:20:32 +0200

    Log client error

--- fastify.js
+++ fastify.js
@@ -368,6 +368,7 @@ function build (options) {
       message: 'Client Error',
       statusCode: 400
     })
+    logger.logger.error(e, 'client error')
     socket.end(`HTTP/1.1 400 Bad Request\r\nContent-Length: ${body.length}\r\nContent-Type: 'application/json'\r\n\r\n${body}`)
   }
 

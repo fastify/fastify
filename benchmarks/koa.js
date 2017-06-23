eb23bb8477f782a5a98c158cc0b6e5f515b41629
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-23 16:11:38 +0200

    Upgrade deprecated koa middleware signature

--- benchmarks/koa.js
+++ benchmarks/koa.js
@@ -3,8 +3,8 @@
 var Koa = require('koa')
 var app = new Koa()
 
-app.use(function * () {
-  this.body = JSON.stringify({ hello: 'world' })
+app.use(async (ctx) => {
+  ctx.body = JSON.stringify({ hello: 'world' })
 })
 
 app.listen(3000)

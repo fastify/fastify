cf8e2119342b05f8140132ae08eac374b013f968
Matteo Collina <hello@matteocollina.com>
2017-07-04 11:15:20 +0200

    more examples and bench

---
+++ examples/simple.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const fastify = require('../fastify')()
+
+const schema = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
+
+fastify
+  .get('/', schema, function (req, reply) {
+    reply
+      .send({ hello: 'world' })
+  })
+
+fastify.listen(3000, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})

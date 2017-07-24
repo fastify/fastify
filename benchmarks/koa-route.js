9cf72c08128609ad0b5d99fee6bb2d0fc310f1cc
Fionn Kelleher <fionnkell@gmail.com>
2017-07-24 13:59:46 +0100

    Add koa-route benchmark

--- benchmarks/koa-route.js
+++ benchmarks/koa-route.js
@@ -1,10 +1,11 @@
 'use strict'
 
 var Koa = require('koa')
+var _ = require('koa-route')
 var app = new Koa()
 
-app.use(async (ctx) => {
+app.use(_.get('/', async (ctx) => {
   ctx.body = JSON.stringify({ hello: 'world' })
-})
+}))
 
 app.listen(3000)

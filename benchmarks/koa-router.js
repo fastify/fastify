d5baeaee726ce33185ab557b7310423a4eac260c
Fionn Kelleher <fionnkell@gmail.com>
2017-07-28 11:34:41 +0100

    koa-route benchmark => koa-router

--- benchmarks/koa-router.js
+++ benchmarks/koa-router.js
@@ -1,11 +1,14 @@
 'use strict'
 
 var Koa = require('koa')
-var _ = require('koa-route')
+var router = require('koa-router')()
 var app = new Koa()
 
-app.use(_.get('/', async (ctx) => {
+router.get('/', async (ctx) => {
   ctx.body = JSON.stringify({ hello: 'world' })
-}))
+})
+
+app
+  .use(router.routes())
 
 app.listen(3000)

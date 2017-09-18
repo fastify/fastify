29a87363f2db1bebc2b55c6695f118bca603d46d
Matteo Collina <hello@matteocollina.com>
2017-09-18 20:03:00 +0200

    avoid sending when an async function resolve in undefined

--- test/async-await.js
+++ test/async-await.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const request = require('request')
-const fastify = require('..')()
+const Fastify = require('..')
+const fastify = Fastify()
 const sleep = require('then-sleep')
 
 const opts = {
@@ -74,6 +75,31 @@ function asyncTest (t) {
         t.strictEqual(response.statusCode, 200)
         t.strictEqual(response.headers['content-length'], '' + body.length)
         t.deepEqual(JSON.parse(body), { hello: 'world' })
+      })
+    })
+  })
+
+  test('do nothing on undefined', t => {
+    t.plan(4)
+
+    const server = Fastify()
+    const payload = { hello: 'world' }
+
+    server.get('/', async function awaitMyFunc (req, reply) {
+      reply.send(payload)
+    })
+
+    t.tearDown(server.close.bind(server))
+
+    server.listen(0, (err) => {
+      t.error(err)
+      request({
+        method: 'GET',
+        uri: 'http://localhost:' + server.server.address().port + '/'
+      }, (err, res, body) => {
+        t.error(err)
+        t.deepEqual(payload, JSON.parse(body))
+        t.strictEqual(res.statusCode, 200)
       })
     })
   })

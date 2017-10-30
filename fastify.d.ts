fa5a54a31ac5a1da0713213a230f239fbed3f5bb
Tiago Relvao <tiago@century.tech>
2017-10-30 17:30:38 +0000

    typescript definitions: cleanup the listen method definitions
    hostname is not a valid option when using a socket path. backlog doesn't seem to be supported at all.

--- fastify.d.ts
+++ fastify.d.ts
@@ -215,16 +215,7 @@ declare namespace fastify {
      * internally waits for the .ready() event. The callback is the same as the
      * Node core.
      */
-    listen(port: number, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
-    listen(path: string, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
-
-    /**
-     * Starts the server on the given port after all the plugins are loaded,
-     * internally waits for the .ready() event. The callback is the same as the
-     * Node core.
-     */
     listen(port: number, hostname: string, callback?: (err: Error) => void): http.Server
-    listen(path: string, hostname: string, callback?: (err: Error) => void): http.Server
 
     /**
      * Starts the server on the given port after all the plugins are loaded,

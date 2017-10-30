7281fd25ae5601feef75f23654c83590e4b40fbe
Tiago Relvao <tiago@century.tech>
2017-10-30 17:17:04 +0000

    change typescript defs to use an overloaded function for socketPath

--- fastify.d.ts
+++ fastify.d.ts
@@ -5,8 +5,6 @@
 import * as http from 'http';
 import * as pino from 'pino';
 
-type socketPath = string;
-
 declare function fastify(opts?: fastify.ServerOptions): fastify.FastifyInstance;
 
 declare namespace fastify {
@@ -217,21 +215,24 @@ declare namespace fastify {
      * internally waits for the .ready() event. The callback is the same as the
      * Node core.
      */
-    listen(port: number | socketPath, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
+    listen(port: number, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
+    listen(path: string, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
 
     /**
      * Starts the server on the given port after all the plugins are loaded,
      * internally waits for the .ready() event. The callback is the same as the
      * Node core.
      */
-    listen(port: number | socketPath, hostname: string, callback?: (err: Error) => void): http.Server
+    listen(port: number, hostname: string, callback?: (err: Error) => void): http.Server
+    listen(path: string, hostname: string, callback?: (err: Error) => void): http.Server
 
     /**
      * Starts the server on the given port after all the plugins are loaded,
      * internally waits for the .ready() event. The callback is the same as the
      * Node core.
      */
-    listen(port: number | socketPath, callback?: (err: Error) => void): http.Server
+    listen(port: number, callback?: (err: Error) => void): http.Server
+    listen(path: string, callback?: (err: Error) => void): http.Server
 
     /**
      * Registers a listener function that is invoked when all the plugins have

9f61c842e38971c5f4e37a2c913bb84b5802876d
Matteo Collina <hello@matteocollina.com>
2017-10-31 09:02:09 +0100

    Added a post test.

--- test/http2/secure-with-fallback.js
+++ test/http2/secure-with-fallback.js
@@ -28,9 +28,43 @@ fastify.get('/', function (req, reply) {
   reply.code(200).send(msg)
 })
 
+fastify.post('/', function (req, reply) {
+  reply.code(200).send(req.body)
+})
+
+fastify.get('/error', async function (req, reply) {
+  throw new Error('kaboom')
+})
+
 fastify.listen(0, err => {
   t.error(err)
   fastify.server.unref()
+
+  test('https get error', async (t) => {
+    t.plan(1)
+
+    const url = `https://localhost:${fastify.server.address().port}/error`
+    const res = await h2url.concat({ url })
+
+    t.strictEqual(res.headers[':status'], 500)
+  })
+
+  test('https post', async (t) => {
+    t.plan(2)
+
+    const url = `https://localhost:${fastify.server.address().port}`
+    const res = await h2url.concat({
+      url,
+      method: 'POST',
+      body: JSON.stringify({ hello: 'http2' }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+
+    t.strictEqual(res.headers[':status'], 200)
+    t.deepEqual(JSON.parse(res.body), { hello: 'http2' })
+  })
 
   test('https get request', async (t) => {
     t.plan(3)
@@ -54,6 +88,18 @@ fastify.listen(0, err => {
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-length'], '' + body.length)
       t.deepEqual(JSON.parse(body), { hello: 'world' })
+    })
+  })
+
+  test('http1 get error', (t) => {
+    t.plan(2)
+    sget({
+      method: 'GET',
+      url: 'https://localhost:' + fastify.server.address().port + '/error',
+      rejectUnauthorized: false
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 500)
     })
   })
 })

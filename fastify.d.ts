8d0967b933e2a180fbd5ccf64095afb8737b0798
Tiago Relvao <relvao@gmail.com>
2017-11-07 14:55:43 +0000

    add socketPath as an option to the listen method (#403)
    * add socketPath as an option to the listen method

--- fastify.d.ts
+++ fastify.d.ts
@@ -217,13 +217,6 @@ declare namespace fastify {
      * internally waits for the .ready() event. The callback is the same as the
      * Node core.
      */
-    listen(port: number, hostname: string, backlog: number, callback?: (err: Error) => void): http.Server
-
-    /**
-     * Starts the server on the given port after all the plugins are loaded,
-     * internally waits for the .ready() event. The callback is the same as the
-     * Node core.
-     */
     listen(port: number, hostname: string, callback?: (err: Error) => void): http.Server
 
     /**
@@ -232,6 +225,7 @@ declare namespace fastify {
      * Node core.
      */
     listen(port: number, callback?: (err: Error) => void): http.Server
+    listen(path: string, callback?: (err: Error) => void): http.Server
 
     /**
      * Registers a listener function that is invoked when all the plugins have

8cb448cb22c597a7faeab498dcef0abb89fa7f11
delvedor <tommydelved@gmail.com>
2017-11-16 15:00:45 +0100

    Updated test

--- test/stream.test.js
+++ test/stream.test.js
@@ -4,40 +4,29 @@ const t = require('tap')
 const test = t.test
 const sget = require('simple-get').concat
 const fs = require('fs')
-const fastify = require('..')()
+const resolve = require('path').resolve
+const zlib = require('zlib')
+const pump = require('pump')
+const Fastify = require('..')
 
 test('should respond with a stream', t => {
-  t.plan(1)
-  try {
-    fastify.get('/', function (req, reply) {
-      const stream = fs.createReadStream(process.cwd() + '/test/stream.test.js', 'utf8')
-      reply.code(200).send(stream)
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
-})
+  t.plan(7)
+  const fastify = Fastify()
 
-test('should respond with a stream', t => {
-  t.plan(1)
-  try {
-    fastify.get('/error', function (req, reply) {
-      const stream = fs.createReadStream('not-existing-file', 'utf8')
-      reply.code(200).send(stream)
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
-})
+  fastify.get('/', function (req, reply) {
+    const stream = fs.createReadStream(process.cwd() + '/test/stream.test.js', 'utf8')
+    reply.code(200).send(stream)
+  })
 
-fastify.listen(0, err => {
-  t.error(err)
-  fastify.server.unref()
+  fastify.get('/error', function (req, reply) {
+    const stream = fs.createReadStream('not-existing-file', 'utf8')
+    reply.code(200).send(stream)
+  })
 
-  test('should get a stream response', t => {
-    t.plan(3)
+  fastify.listen(0, err => {
+    t.error(err)
+    fastify.server.unref()
+
     sget(`http://localhost:${fastify.server.address().port}`, function (err, response) {
       t.error(err)
       t.strictEqual(response.headers['content-type'], 'application/octet-stream')
@@ -50,14 +39,40 @@ fastify.listen(0, err => {
         t.pass('Correctly close the stream')
       })
     })
-  })
 
-  test('should get an errored stream response', t => {
-    t.plan(3)
     sget(`http://localhost:${fastify.server.address().port}/error`, function (err, response) {
       t.type(err, Error)
       t.equal(err.code, 'ECONNRESET')
       t.pass('Correctly close the stream')
     })
+  })
+})
+
+test('onSend hook stream', t => {
+  t.plan(3)
+  const fastify = Fastify()
+
+  fastify.get('/', function (req, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.addHook('onSend', (req, reply, payload, next) => {
+    reply.header('Content-Encoding', 'gzip')
+    next(null, pump(
+      fs.createReadStream(resolve(process.cwd() + '/test/stream.test.js'), 'utf8'),
+      zlib.createGzip(),
+      t.error
+    ))
+  })
+
+  fastify.inject({
+    url: '/',
+    method: 'GET'
+  }, res => {
+    t.strictEqual(res.headers['content-encoding'], 'gzip')
+    const file = fs.readFileSync(resolve(process.cwd() + '/test/stream.test.js'), 'utf8')
+    const payload = zlib.gunzipSync(res.rawPayload)
+    t.strictEqual(payload.toString('utf-8'), file)
+    fastify.close()
   })
 })

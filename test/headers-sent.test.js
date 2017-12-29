c10af36e8f28d61b7940897267f62a1f98500936
Gilles De Mey <gilles.de.mey@gmail.com>
2017-12-29 01:24:05 +0100

    adds basic crash test

---
+++ test/headers-sent.test.js
@@ -0,0 +1,38 @@
+const spawn = require('child_process').spawn
+const path = require('path')
+const tap = require('tap')
+
+const code = `
+const Fastify = require('./')
+const fs = require('fs')
+const fastify = Fastify()
+const sget = require('simple-get')
+
+fastify.get('/', (req, reply) => {
+  const endlessStream = fs.createReadStream('/dev/random')
+  reply.send(endlessStream)
+})
+
+fastify.listen(0, err => {
+  fastify.server.unref()
+
+  const req = sget(\`http://localhost:\${fastify.server.address().port}\`, (err, response) => {
+    req.abort()
+  })
+})
+`
+tap.test('disconnecting from a stream response should not crash the server', t => {
+  t.plan(1)
+
+  const server = spawn('node', ['-e', code], {
+    cwd: path.resolve()
+  })
+
+  server.stderr.on('data', err => {
+    t.fail(err.toString())
+  })
+
+  server.on('close', code => {
+    t.equal(code, 0, 'non-zero exit code')
+  })
+})

fastify/fastify

commit f545accb2976d54a63a573cc187a0b0472cba972

Avoid to log if the log is not set (#776)

fastify.js
@@ -34,6 +34,8 @@ function validateBodyLimitOption (bodyLimit) {
   }
 }
 
+function noop () { }
+
 function build (options) {
   options = options || {}
   if (typeof options !== 'object') {
@@ -41,12 +43,14 @@ function build (options) {
   }
 
   var log
+  var hasLogger = false
   if (isValidLogger(options.logger)) {
     log = loggerUtils.createLogger({
       logger: options.logger,
       serializers: Object.assign({}, loggerUtils.serializers, options.logger.serializers)
     })
   } else if (!options.logger) {
+    hasLogger = true
     log = Object.create(abstractLogging)
     log.child = () => log
   } else {
@@ -72,7 +76,7 @@ function build (options) {
   const genReqId = customGenReqId || loggerUtils.reqIdGenFactory()
   const now = loggerUtils.now
   const onResponseIterator = loggerUtils.onResponseIterator
-  const onResponseCallback = loggerUtils.onResponseCallback
+  const onResponseCallback = hasLogger ? noop : loggerUtils.onResponseCallback
 
   const app = avvio(fastify, {
     autostart: false
@@ -195,16 +199,19 @@ function build (options) {
   return fastify
 
   function routeHandler (req, res, params, context) {
-    res._context = context
     req.id = genReqId(req)
     req.log = res.log = log.child({ reqId: req.id, level: context.logLevel })
     req.originalUrl = req.url
 
     req.log.info({ req }, 'incoming request')
 
-    res._startTime = now()
-    res.on('finish', onResFinished)
-    res.on('error', onResFinished)
+    res._startTime = hasLogger ? now() : undefined
+
+    if (hasLogger === false || context.onResponse !== null) {
+      res._context = context
+      res.on('finish', onResFinished)
+      res.on('error', onResFinished)
+    }
 
     if (context.onRequest !== null) {
       runHooks(

commit 1b9986e3e009e65c1f1b7407ab982f20557f19bb

fix responseTime regression
Introduced in https://github.com/fastify/fastify/pull/776

Fixes https://github.com/fastify/fastify/issues/826

fastify.js
@@ -43,14 +43,14 @@ function build (options) {
   }
 
   var log
-  var hasLogger = false
+  var hasLogger = true
   if (isValidLogger(options.logger)) {
     log = loggerUtils.createLogger({
       logger: options.logger,
       serializers: Object.assign({}, loggerUtils.serializers, options.logger.serializers)
     })
   } else if (!options.logger) {
-    hasLogger = true
+    hasLogger = false
     log = Object.create(abstractLogging)
     log.child = () => log
   } else {
@@ -76,7 +76,7 @@ function build (options) {
   const genReqId = customGenReqId || loggerUtils.reqIdGenFactory()
   const now = loggerUtils.now
   const onResponseIterator = loggerUtils.onResponseIterator
-  const onResponseCallback = hasLogger ? noop : loggerUtils.onResponseCallback
+  const onResponseCallback = hasLogger ? loggerUtils.onResponseCallback : noop
 
   const app = avvio(fastify, {
     autostart: false
@@ -202,13 +202,12 @@ function build (options) {
     req.id = genReqId(req)
     req.log = res.log = log.child({ reqId: req.id, level: context.logLevel })
     req.originalUrl = req.url
+    res._startTime = hasLogger ? now() : undefined
+    res._context = context
 
     req.log.info({ req }, 'incoming request')
 
-    res._startTime = hasLogger ? now() : undefined
-
-    if (hasLogger === false || context.onResponse !== null) {
-      res._context = context
+    if (hasLogger === true || context.onResponse !== null) {
       res.on('finish', onResFinished)
       res.on('error', onResFinished)
     }

lib/logger.js
@@ -65,7 +65,11 @@ function now () {
 }
 
 function onResponseCallback (err, res) {
-  var responseTime = now() - res._startTime
+  var responseTime = 0
+
+  if (res._startTime) {
+    responseTime = now() - res._startTime
+  }
 
   if (err) {
     res.log.error({

test/internals/logger.test.js
@@ -3,6 +3,8 @@
 const t = require('tap')
 const test = t.test
 const Fastify = require('../..')
+const split = require('split2')
+const proxyquire = require('proxyquire')
 const loggerUtils = require('../../lib/logger')
 
 test('time resolution', t => {
@@ -69,6 +71,39 @@ test('The logger should reuse request id header for req.id', t => {
       fastify.close()
       t.end()
     })
+  })
+})
+
+test('The logger should add a timestamp if logging to stdout', t => {
+  t.plan(5)
+
+  const stream = split(JSON.parse)
+  const stub = Object.create(loggerUtils)
+  stub.createLogger = function (opts) {
+    return loggerUtils.createLogger(opts, stream)
+  }
+
+  const Fastify = proxyquire('../..', {
+    './lib/logger': stub
+  })
+  const fastify = Fastify({ logger: true })
+
+  stream.once('data', (line) => {
+    t.equal(line.msg, 'incoming request')
+    stream.once('data', (line) => {
+      t.equal(line.msg, 'Not found')
+      stream.once('data', (line) => {
+        t.equal(line.msg, 'request completed')
+        t.ok(line.responseTime, 'responseTime exists')
+      })
+    })
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/'
+  }, function (err, res) {
+    t.error(err)
   })
 })
 

test/logger.test.js
@@ -10,7 +10,7 @@ const pino = require('pino')
 const os = require('os')
 
 test('defaults to info level', t => {
-  t.plan(12)
+  t.plan(13)
   var fastify = null
   var stream = split(JSON.parse)
   try {
@@ -28,30 +28,32 @@ test('defaults to info level', t => {
     reply.send({ hello: 'world' })
   })
 
+  stream.once('data', listenAtLogLine => {
+    t.ok(listenAtLogLine, 'listen at log message is ok')
+
+    stream.once('data', line => {
+      const id = line.reqId
+      t.ok(line.reqId, 'reqId is defined')
+      t.ok(line.req, 'req is defined')
+      t.equal(line.msg, 'incoming request', 'message is set')
+      t.equal(line.req.method, 'GET', 'method is get')
+
+      stream.once('data', line => {
+        t.equal(line.reqId, id)
+        t.ok(line.reqId, 'reqId is defined')
+        t.ok(line.res, 'res is defined')
+        t.equal(line.msg, 'request completed', 'message is set')
+        t.equal(line.res.statusCode, 200, 'statusCode is 200')
+        t.ok(line.responseTime, 'responseTime is defined')
+      })
+    })
+  })
+
   fastify.listen(0, err => {
     t.error(err)
     fastify.server.unref()
 
     http.get('http://localhost:' + fastify.server.address().port)
-    stream.once('data', listenAtLogLine => {
-      t.ok(listenAtLogLine, 'listen at log message is ok')
-
-      stream.once('data', line => {
-        const id = line.reqId
-        t.ok(line.reqId, 'reqId is defined')
-        t.ok(line.req, 'req is defined')
-        t.equal(line.msg, 'incoming request', 'message is set')
-        t.equal(line.req.method, 'GET', 'method is get')
-
-        stream.once('data', line => {
-          t.equal(line.reqId, id)
-          t.ok(line.reqId, 'reqId is defined')
-          t.ok(line.res, 'res is defined')
-          t.equal(line.msg, 'request completed', 'message is set')
-          t.equal(line.res.statusCode, 200, 'statusCode is 200')
-        })
-      })
-    })
   })
 })
 
850080d1cfa06bf4694f813f65e92ea5589db62c
Jimmy Hurrah <jimmy.hurrah@gmail.com>
2018-04-27 00:49:47 +0200

    convert async/await test to callback

--- test/input-validation.test.js
+++ test/input-validation.test.js
@@ -4,34 +4,32 @@ const t = require('tap')
 const test = t.test
 const fastify = require('..')()
 
-test('case insensitive header validation', async t => {
-  t.plan(1)
-  try {
-    fastify.route({
-      method: 'GET',
-      url: '/',
-      handler: (req, reply) => {
-        reply.code(200).send(req.headers.foobar)
-      },
-      schema: {
-        headers: {
-          type: 'object',
-          required: ['FooBar'],
-          properties: {
-            FooBar: { type: 'string' }
-          }
+test('case insensitive header validation', t => {
+  t.plan(2)
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: (req, reply) => {
+      reply.code(200).send(req.headers.foobar)
+    },
+    schema: {
+      headers: {
+        type: 'object',
+        required: ['FooBar'],
+        properties: {
+          FooBar: { type: 'string' }
         }
       }
-    })
-    const res = await fastify.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        'FooBar': 'Baz'
-      }
-    })
-    t.equal(res.payload, 'Baz')
-  } catch (err) {
+    }
+  })
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'FooBar': 'Baz'
+    }
+  }, (err, res) => {
     t.error(err)
-  }
+    t.equal(res.payload, 'Baz')
+  })
 })

ff99b212789515644eee79d298f69670ed182a32
Dustin Deus <deusdustin@gmail.com>
2018-05-13 20:39:39 +0200

    statusCode is optional

--- test/internals/errors.test.js
+++ test/internals/errors.test.js
@@ -16,6 +16,18 @@ test('Create error', t => {
   t.ok(err.stack)
 })
 
+test('Create error with no statusCode property', t => {
+  t.plan(6)
+  const NewError = createError('CODE', 'hey %s', 0)
+  const err = new NewError('dude')
+  t.type(err, Error)
+  t.equal(err.name, 'FastifyError [CODE]')
+  t.equal(err.message, 'Code: CODE; hey dude')
+  t.equal(err.code, 'CODE')
+  t.notOk(err.statusCode)
+  t.ok(err.stack)
+})
+
 test('Create error with different base', t => {
   t.plan(7)
   const NewError = createError('CODE', 'hey %s', 500, TypeError)

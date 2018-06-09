57c172af1f0e628ce1e15b818c2f716516ace433
starptech <deusdustin@gmail.com>
2018-06-09 12:55:22 +0200

    add tests for fixed count of args in errors

--- test/internals/errors.test.js
+++ test/internals/errors.test.js
@@ -4,13 +4,37 @@ const t = require('tap')
 const test = t.test
 const { createError } = require('../../lib/errors')
 
-test('Create error', t => {
+test('Create error with 1 parameter', t => {
   t.plan(6)
   const NewError = createError('CODE', 'hey %s')
-  const err = new NewError('dude')
+  const err = new NewError('alice')
   t.type(err, Error)
   t.equal(err.name, 'FastifyError [CODE]')
-  t.equal(err.message, 'CODE: hey dude')
+  t.equal(err.message, 'CODE: hey alice')
+  t.equal(err.code, 'CODE')
+  t.equal(err.statusCode, 500)
+  t.ok(err.stack)
+})
+
+test('Create error with 2 parameters', t => {
+  t.plan(6)
+  const NewError = createError('CODE', 'hey %s, I like your %s')
+  const err = new NewError('alice', 'attitude')
+  t.type(err, Error)
+  t.equal(err.name, 'FastifyError [CODE]')
+  t.equal(err.message, 'CODE: hey alice, I like your attitude')
+  t.equal(err.code, 'CODE')
+  t.equal(err.statusCode, 500)
+  t.ok(err.stack)
+})
+
+test('Create error with 3 parameters', t => {
+  t.plan(6)
+  const NewError = createError('CODE', 'hey %s, I like your %s %s')
+  const err = new NewError('alice', 'attitude', 'see you')
+  t.type(err, Error)
+  t.equal(err.name, 'FastifyError [CODE]')
+  t.equal(err.message, 'CODE: hey alice, I like your attitude see you')
   t.equal(err.code, 'CODE')
   t.equal(err.statusCode, 500)
   t.ok(err.stack)

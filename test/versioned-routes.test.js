0391d4b796ab78e8e692eb11fd903d2dc27927f1
delvedor <tommydelved@gmail.com>
2018-06-13 11:40:40 +0200

    Updated test

--- test/versioned-routes.test.js
+++ test/versioned-routes.test.js
@@ -6,7 +6,7 @@ const Fastify = require('..')
 const sget = require('simple-get').concat
 
 test('Should register a versioned route', t => {
-  t.plan(5)
+  t.plan(11)
   const fastify = Fastify()
 
   fastify.route({
@@ -23,6 +23,30 @@ test('Should register a versioned route', t => {
     url: '/',
     headers: {
       'Accept-Version': '1.x'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.deepEqual(JSON.parse(res.payload), { hello: 'world' })
+    t.strictEqual(res.statusCode, 200)
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'Accept-Version': '1.2.x'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.deepEqual(JSON.parse(res.payload), { hello: 'world' })
+    t.strictEqual(res.statusCode, 200)
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'Accept-Version': '1.2.0'
     }
   }, (err, res) => {
     t.error(err)
@@ -258,5 +282,137 @@ test('The not found handler should not use the Accept-Version header', t => {
     t.error(err)
     t.deepEqual(res.payload, 'not found handler')
     t.strictEqual(res.statusCode, 404)
+  })
+})
+
+test('Bad accept version (inject)', t => {
+  t.plan(4)
+  const fastify = Fastify()
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    version: '1.2.0',
+    handler: (req, reply) => {
+      reply.send({ hello: 'world' })
+    }
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'Accept-Version': 'a.b.c'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.strictEqual(res.statusCode, 404)
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'Accept-Version': 12
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.strictEqual(res.statusCode, 404)
+  })
+})
+
+test('Bas accept version (server)', t => {
+  t.plan(5)
+  const fastify = Fastify()
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    version: '1.2.0',
+    handler: (req, reply) => {
+      reply.send({ hello: 'world' })
+    }
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+    fastify.server.unref()
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port,
+      headers: {
+        'Accept-Version': 'a.b.c'
+      }
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 404)
+    })
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port,
+      headers: {
+        'Accept-Version': 12
+      }
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 404)
+    })
+  })
+})
+
+test('encapsulated 500', t => {
+  t.plan(8)
+
+  const fastify = Fastify()
+
+  fastify.get('/', { version: '1.2.0' }, function (req, reply) {
+    reply.send(new Error('kaboom'))
+  })
+
+  fastify.register(function (f, opts, next) {
+    f.get('/', { version: '1.3.0' }, function (req, reply) {
+      reply.send(new Error('kaboom'))
+    })
+
+    f.setErrorHandler(function (err, request, reply) {
+      reply
+        .code(500)
+        .type('text/plain')
+        .send('an error happened: ' + err.message)
+    })
+
+    next()
+  }, { prefix: 'test' })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/test',
+    headers: {
+      'Accept-Version': '1.3.0'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.strictEqual(res.statusCode, 500)
+    t.strictEqual(res.headers['content-type'], 'text/plain')
+    t.deepEqual(res.payload.toString(), 'an error happened: kaboom')
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      'Accept-Version': '1.2.0'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.strictEqual(res.statusCode, 500)
+    t.strictEqual(res.headers['content-type'], 'application/json')
+    t.deepEqual(JSON.parse(res.payload), {
+      error: 'Internal Server Error',
+      message: 'kaboom',
+      statusCode: 500
+    })
   })
 })

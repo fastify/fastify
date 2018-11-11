a010b27bcf16f99a4f365647a686990184fa2824
cemremengu <cemremengu@gmail.com>
2018-11-12 01:49:36 +0300

    Better error message for empty body application/json

--- test/post.test.js
+++ test/post.test.js
@@ -23,3 +23,77 @@ t.test('cannot set schemaCompiler after binding', t => {
     }
   })
 })
+
+t.test('should error with empty body and application/json content-type', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.post('/', function (req, reply) {
+    reply.code(200).send(req.body.name)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    url: '/'
+  }, (err, res) => {
+    t.error(err)
+    const payload = JSON.parse(res.payload)
+    t.is(payload.error, `Bad Request`)
+    t.is(payload.message, `Body cannot be empty when content-type is set to 'application/json'`)
+    t.strictEqual(res.statusCode, 400)
+  })
+})
+
+t.test('should error with null body and application/json content-type', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.post('/', function (req, reply) {
+    reply.code(200).send(req.body.name)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    payload: null,
+    url: '/'
+  }, (err, res) => {
+    t.error(err)
+    const payload = JSON.parse(res.payload)
+    t.is(payload.error, `Bad Request`)
+    t.is(payload.message, `Body cannot be empty when content-type is set to 'application/json'`)
+    t.strictEqual(res.statusCode, 400)
+  })
+})
+
+t.test('should error with undefined body and application/json content-type', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.post('/', function (req, reply) {
+    reply.code(200).send(req.body.name)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    payload: undefined,
+    url: '/'
+  }, (err, res) => {
+    t.error(err)
+    const payload = JSON.parse(res.payload)
+    t.is(payload.error, `Bad Request`)
+    t.is(payload.message, `Body cannot be empty when content-type is set to 'application/json'`)
+    t.strictEqual(res.statusCode, 400)
+  })
+})

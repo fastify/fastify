9f9c88fed17c99675074ef095bd324515bd6c9b9
Manuel Spigolon <behemoth89@gmail.com>
2018-12-26 11:00:12 +0100

    fix: #1353 ignore evaluation of $schema field in json-schema (#1354)

--- test/input-validation.test.js
+++ test/input-validation.test.js
@@ -2,10 +2,11 @@
 
 const t = require('tap')
 const test = t.test
-const fastify = require('..')()
+const Fastify = require('..')
 
 test('case insensitive header validation', t => {
   t.plan(2)
+  const fastify = Fastify()
   fastify.route({
     method: 'GET',
     url: '/',
@@ -31,5 +32,38 @@ test('case insensitive header validation', t => {
   }, (err, res) => {
     t.error(err)
     t.equal(res.payload, 'Baz')
+  })
+})
+
+test('not evaluate json-schema $schema keyword', t => {
+  t.plan(2)
+  const fastify = Fastify()
+  fastify.route({
+    method: 'POST',
+    url: '/',
+    handler: (req, reply) => {
+      reply.code(200).send(req.body.hello)
+    },
+    schema: {
+      body: {
+        $schema: 'http://json-schema.org/draft-07/schema#',
+        type: 'object',
+        properties: {
+          hello: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  })
+  fastify.inject({
+    method: 'POST',
+    url: '/',
+    body: {
+      hello: 'world'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.equal(res.payload, 'world')
   })
 })

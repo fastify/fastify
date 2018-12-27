983d70b1186ee6655fcdc7b0eb8540933d2b47f2
Tommaso Allevi <tomallevi@gmail.com>
2018-12-27 20:25:46 +0100

    Use license-checker option for checks

--- tools/license-checker.sh
+++ tools/license-checker.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 
+set -e
 
-unknownLicenses=`license-checker --production --csv | grep -v '"MIT"' | grep -v '"ISC"' | grep -v '"BSD-3-Clause"' | grep -v '"BSD-2-Clause"'`
-unknownLicenseCount=`echo "$unknownLicenses" | wc -l`
+license-checker --production --onlyAllow='MIT;ISC;BSD-3-Clause;BSD-2-Clause'
 
-if [ "$unknownLicenseCount" -eq 1 ]; then
-  echo 'Licenses are OK'
-else
-  echo 'Unknown License!'
-  echo "$unknownLicenses"
-  exit 1
-fi

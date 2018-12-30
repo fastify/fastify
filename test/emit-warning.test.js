8b0b42c3c5fb819170dcce6c402a40c398a413df
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-12-29 19:08:43 -0500

    use event listeners instead of monkey-patching

--- test/emit-warning.test.js
+++ test/emit-warning.test.js
@@ -5,17 +5,21 @@ const Fastify = require('..')
 
 test('should emit warning using genReqId prop in logger options', t => {
   t.plan(1)
-  process.emitWarning = function (string) {
-    t.strictEqual(string, `Using 'genReqId' in logger options is deprecated. Use fastify options instead. See: https://www.fastify.io/docs/latest/Server/#gen-request-id`)
-  }
+
+  process.once('warning', warning => {
+    t.strictEqual(warning.message, `Using 'genReqId' in logger options is deprecated. Use fastify options instead. See: https://www.fastify.io/docs/latest/Server/#gen-request-id`)
+  })
+
   Fastify({ logger: { genReqId: 'test' } })
 })
 
 test('should emit warning if basePath prop is used', t => {
   t.plan(1)
-  process.emitWarning = function (string) {
-    t.strictEqual(string, `basePath is deprecated. Use prefix instead. See: https://www.fastify.io/docs/latest/Server/#prefix`)
-  }
+
+  process.once('warning', warning => {
+    t.strictEqual(warning.message, `basePath is deprecated. Use prefix instead. See: https://www.fastify.io/docs/latest/Server/#prefix`)
+  })
+
   const fastify = Fastify({ basePath: '/test' })
   return fastify.basePath
 })

fd2dc5b496fceea1471cdc890b91658d1738894c
flickz <omoyajowo2015@gmail.com>
2019-01-11 11:59:17 +0100

    Add assertion to error call back

--- test/wrapThenable.test.js
+++ test/wrapThenable.test.js
@@ -6,23 +6,23 @@ const { kReplySentOverwritten } = require('../lib/symbols')
 const wrapThenable = require('../lib/wrapThenable')
 
 test('should resolve immediately when reply[kReplySentOverwritten] is true', t => {
-  t.plan(1)
   const reply = {}
   reply[kReplySentOverwritten] = true
   const thenable = Promise.resolve()
   wrapThenable(thenable, reply)
-  t.pass()
+  t.end()
 })
 
-test('should reject immediately when reply.sentOverwritten is true', t => {
+test('should reject immediately when reply[kReplySentOverwritten] is true', t => {
   t.plan(1)
   const reply = { res: {} }
   reply[kReplySentOverwritten] = true
   reply.res.log = {
-    error: () => { }
+    error: ({ err }) => {
+      t.strictEqual(err.message, 'Reply sent already')
+    }
   }
 
   const thenable = Promise.reject(new Error('Reply sent already'))
   wrapThenable(thenable, reply)
-  t.pass()
 })

dd79c2261ecca87c85391da943a0dfc6e58de6a2
Manuel Spigolon <behemoth89@gmail.com>
2019-02-01 00:43:18 +0100

    fix: json schema $ref with $id set in definitions

--- test/shared-schemas.test.js
+++ test/shared-schemas.test.js
@@ -441,6 +441,47 @@ test('Use the same schema id in diferent places', t => {
   })
 })
 
+test('Use shared schema and $ref with $id', t => {
+  t.plan(1)
+  const fastify = Fastify()
+
+  fastify.addSchema({
+    $id: 'test',
+    type: 'object',
+    properties: {
+      id: { type: 'number' }
+    }
+  })
+
+  const body = {
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    $id: 'http://foo/user',
+    type: 'object',
+    definitions: {
+      address: {
+        $id: '#address',
+        type: 'object',
+        properties: {
+          city: { 'type': 'string' }
+        }
+      }
+    },
+    properties: {
+      test: 'test#',
+      address: { $ref: '#address' }
+    }
+  }
+
+  fastify.route({
+    method: 'POST',
+    url: '/',
+    schema: { body },
+    handler: () => {}
+  })
+
+  fastify.ready(t.error)
+})
+
 // https://github.com/fastify/fastify/issues/1043
 test('The schema resolver should clean the $id key before passing it to the compiler', t => {
   t.plan(1)

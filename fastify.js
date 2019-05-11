57c5e3df08ae03b02afa4836ea8fd049fc344187
Manuel Spigolon <behemoth89@gmail.com>
2019-05-11 22:22:02 +0200

    renamed method

--- fastify.js
+++ fastify.js
@@ -251,7 +251,7 @@ function build (options) {
   fastify.setNotFoundHandler()
   fourOhFour.arrange404(fastify)
 
-  router.fill(options, {
+  router.setup(options, {
     avvio,
     fourOhFour,
     logger,

a4da46ecddd5d06d2973fd038c0131ce3bbfbdad
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-05-23 12:49:09 -0400

    udpate schema type and doc

--- types/schema.d.ts
+++ types/schema.d.ts
@@ -1,8 +1,12 @@
-export interface FastifySchema {
-  body: any,
-  querystring: any,
-  params: any,
-  response: any
-} // todo
+/**
+ * Schemas in Fastify follow the JSON-Schema standard. For this reason
+ * we have opted to not ship strict schema based types. Instead we provide
+ * an example in our documentation on how to solve this problem. Check it
+ * out here:
+ */
+export type FastifySchema = object;
 
+/**
+ * Compiler for FastifySchema Type
+ */
 export type FastifySchemaCompiler = (schema: FastifySchema) => void // todo

323616533210083c59a2f11bed99d4c032fe264f
Adriano Raiano <adrai@users.noreply.github.com>
2019-06-18 19:50:14 +0200

    fix nullable validation test

--- test/nullable-validation.test.js
+++ test/nullable-validation.test.js
@@ -5,12 +5,13 @@ const test = t.test
 const Fastify = require('..')
 
 test('nullable string', t => {
-  t.plan(2)
+  t.plan(3)
   const fastify = Fastify()
   fastify.route({
     method: 'POST',
     url: '/',
     handler: (req, reply) => {
+      t.same(req.body.hello, null)
       reply.code(200).send(req.body)
     },
     schema: {

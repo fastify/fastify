bf42cb92b66d4610c329af34cfaf3effa666cf6d
Manuel Spigolon <behemoth89@gmail.com>
2019-09-19 23:34:59 +0200

    add test for body

--- test/schemas.test.js
+++ test/schemas.test.js
@@ -5,13 +5,13 @@ const test = t.test
 const Fastify = require('..')
 
 const AJV = require('ajv')
+const fastClone = require('rfdc')({ circles: false, proto: true })
 
 const schemaUsed = {
   $id: 'urn:schema:foo',
   definitions: {
     foo: { type: 'string' }
   },
-
   type: 'object',
   properties: {
     foo: { $ref: '#/definitions/foo' }
@@ -25,13 +25,13 @@ const schemaParent = {
   }
 }
 
-test('Should use the ref resolver', t => {
+test('Should use the ref resolver - response', t => {
   t.plan(2)
   const fastify = Fastify()
   const ajv = new AJV()
 
-  ajv.addSchema(schemaParent)
-  ajv.addSchema(schemaUsed)
+  ajv.addSchema(fastClone(schemaParent))
+  ajv.addSchema(fastClone(schemaUsed))
 
   fastify.setSchemaResolver((ref) => {
     t.equals(ref, 'urn:schema:foo')
@@ -52,4 +52,40 @@ test('Should use the ref resolver', t => {
   })
 
   fastify.ready(t.error)
+})
+
+test('Should use the ref resolver - body', t => {
+  t.plan(3)
+  const fastify = Fastify()
+  const ajv = new AJV()
+
+  ajv.addSchema(fastClone(schemaParent))
+  ajv.addSchema(fastClone(schemaUsed))
+
+  fastify.setSchemaCompiler(schema => ajv.compile(schema))
+
+  fastify.setSchemaResolver((ref) => {
+    t.equals(ref, 'urn:schema:foo')
+    return ajv.getSchema(ref).schema
+  })
+
+  fastify.route({
+    method: 'POST',
+    url: '/',
+    schema: {
+      body: ajv.getSchema('urn:schema:response').schema
+    },
+    handler (req, reply) {
+      reply.send({ foo: 'bar' })
+    }
+  })
+
+  fastify.inject({
+    method: 'POST',
+    url: '/',
+    payload: { foo: 'bar' }
+  }, (err, res) => {
+    t.error(err)
+    t.deepEquals(JSON.parse(res.payload), { foo: 'bar' })
+  })
 })

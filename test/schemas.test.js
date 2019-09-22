0f83e4f976d80d53715af4e5271e1ac55559726e
Manuel Spigolon <behemoth89@gmail.com>
2019-09-22 18:20:40 +0200

    add encapsulation test

--- test/schemas.test.js
+++ test/schemas.test.js
@@ -20,6 +20,7 @@ const schemaUsed = {
 const schemaParent = {
   $id: 'urn:schema:response',
   type: 'object',
+  required: ['foo'],
   properties: {
     foo: { $ref: 'urn:schema:foo#/definitions/foo' }
   }
@@ -29,7 +30,6 @@ test('Should use the ref resolver - response', t => {
   t.plan(2)
   const fastify = Fastify()
   const ajv = new AJV()
-
   ajv.addSchema(fastClone(schemaParent))
   ajv.addSchema(fastClone(schemaUsed))
 
@@ -58,7 +58,6 @@ test('Should use the ref resolver - body', t => {
   t.plan(3)
   const fastify = Fastify()
   const ajv = new AJV()
-
   ajv.addSchema(fastClone(schemaParent))
   ajv.addSchema(fastClone(schemaUsed))
 
@@ -87,5 +86,108 @@ test('Should use the ref resolver - body', t => {
   }, (err, res) => {
     t.error(err)
     t.deepEquals(JSON.parse(res.payload), { foo: 'bar' })
+  })
+})
+
+test('Encapsulation', t => {
+  t.plan(14)
+  const fastify = Fastify()
+  const ajv = new AJV()
+  ajv.addSchema(fastClone(schemaParent))
+  ajv.addSchema(fastClone(schemaUsed))
+
+  fastify.register((instance, opts, next) => {
+    instance.setSchemaCompiler(schema => ajv.compile(schema))
+    instance.setSchemaResolver((ref) => {
+      return ajv.getSchema(ref).schema
+    })
+    instance.route({
+      method: 'POST',
+      url: '/',
+      schema: {
+        body: ajv.getSchema('urn:schema:response').schema
+      },
+      handler (req, reply) {
+        reply.send({ foo: 'bar' })
+      }
+    })
+
+    instance.register((instance, opts, next) => {
+      instance.route({
+        method: 'POST',
+        url: '/two',
+        schema: {
+          body: ajv.getSchema('urn:schema:response').schema
+        },
+        handler (req, reply) {
+          reply.send({ foo: 'bar' })
+        }
+      })
+      next()
+    })
+    next()
+  })
+
+  fastify.register((instance, opts, next) => {
+    instance.route({
+      method: 'POST',
+      url: '/clean',
+      handler (req, reply) {
+        reply.send({ foo: 'bar' })
+      }
+    })
+    next()
+  })
+
+  fastify.ready(err => {
+    t.error(err)
+
+    fastify.inject({
+      method: 'POST',
+      url: '/',
+      payload: { foo: 'bar' }
+    }, (err, res) => {
+      t.error(err)
+      t.equals(res.statusCode, 200)
+      t.deepEquals(JSON.parse(res.payload), { foo: 'bar' })
+    })
+
+    fastify.inject({
+      method: 'POST',
+      url: '/',
+      payload: { wrongFoo: 'bar' }
+    }, (err, res) => {
+      t.error(err)
+      t.equals(res.statusCode, 400)
+    })
+
+    fastify.inject({
+      method: 'POST',
+      url: '/two',
+      payload: { foo: 'bar' }
+    }, (err, res) => {
+      t.error(err)
+      t.equals(res.statusCode, 200)
+      t.deepEquals(JSON.parse(res.payload), { foo: 'bar' })
+    })
+
+    fastify.inject({
+      method: 'POST',
+      url: '/two',
+      payload: { wrongFoo: 'bar' }
+    }, (err, res) => {
+      t.error(err)
+      t.equals(res.statusCode, 400)
+    })
+
+    fastify.inject({
+      method: 'POST',
+      url: '/clean',
+      payload: { wrongFoo: 'bar' }
+    }, (err, res) => {
+      t.error(err)
+      t.equals(res.statusCode, 200)
+      t.deepEquals(JSON.parse(res.payload), { foo: 'bar' })
+    })
   })
 })

d0fbf31a7ced4e6f444be46c9491bc7ae6001450
Matteo Collina <hello@matteocollina.com>
2019-09-24 08:56:05 +0200

    Make reply a thenable

--- test/async-await.js
+++ test/async-await.js
@@ -205,6 +205,40 @@ function asyncTest (t) {
     })
   })
 
+  test('await reply if we will be calling reply.send in the future', t => {
+    const lines = ['incoming request', 'request completed']
+    t.plan(lines.length + 2)
+
+    const splitStream = split(JSON.parse)
+    splitStream.on('data', (line) => {
+      t.is(line.msg, lines.shift())
+    })
+
+    const server = Fastify({
+      logger: {
+        stream: splitStream
+      }
+    })
+    const payload = { hello: 'world' }
+
+    server.get('/', async function awaitMyFunc (req, reply) {
+      setImmediate(function () {
+        reply.send(payload)
+      })
+
+      await reply
+    })
+
+    server.inject({
+      method: 'GET',
+      url: '/'
+    }, (err, res) => {
+      t.error(err)
+      const payload = JSON.parse(res.payload)
+      t.deepEqual(payload, { hello: 'world' })
+    })
+  })
+
   test('support reply decorators with await', t => {
     t.plan(2)
 

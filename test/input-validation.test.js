469fa161ffae0afba6eda43e7d562aac2c31511a
gendronb <bruno.gendron.consult@gmail.com>
2019-10-27 12:08:53 -0400

    Added test for validation context

--- test/input-validation.test.js
+++ test/input-validation.test.js
@@ -67,3 +67,36 @@ test('not evaluate json-schema $schema keyword', t => {
     t.equal(res.payload, 'world')
   })
 })
+
+test('validation context in validation result', t => {
+  t.plan(2)
+  const fastify = Fastify()
+  // return validation context, so we can test it in response
+  fastify.setErrorHandler((err, request, reply) => reply.send(err.validationContext))
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: (req, reply) => {
+      reply.code(200).send(req.body.hello)
+    },
+    schema: {
+      body: {
+        type: 'object',
+        required: ['hello'],
+        properties: {
+          hello: { type: 'string' }
+        }
+      }
+    }
+  })
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    // body lacks required field, will fail validation
+    body: {}
+  }, (err, res) => {
+    t.error(err)
+    // validationContext returned by custom error handler
+    t.equal(res.payload, 'body')
+  })
+})

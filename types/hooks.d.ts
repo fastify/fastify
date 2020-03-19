aadefb2e5c4451452c03573e0c8e844812864b19
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-03-19 17:56:36 -0400

    Remove middleware types/Fix hook definitions (#2042)
    * temp commit
    
    * another temp commit
    
    * another temp commit. getting closer
    
    * complete hooks update
    
    * change export and remove unused defs
    
    * move overload to instance def. add async compat
    
    * delete middleware types
    
    * fix space in error file oops
    
    * fix bad merge oops

--- types/hooks.d.ts
+++ types/hooks.d.ts
@@ -1,145 +1,216 @@
-import { FastifyMiddleware } from './middleware'
 import { FastifyInstance } from './instance'
 import { RouteOptions } from './route'
-import { RawServerBase, RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression } from './utils'
-import { FastifyRequest } from './request'
+import { RawServerBase, RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression, ContextConfigDefault } from './utils'
+import { FastifyRequest, RequestGenericInterface } from './request'
 import { FastifyReply } from './reply'
 import { FastifyError } from './error'
+import { FastifyLoggerOptions } from './logger'
+
+type HookHandlerDoneFunction = (err?: FastifyError) => void
+
+// Lifecycle Hooks
 
 /**
- * preParsing hook useful for authentication
+ * `onRequest` is the first hook to be executed in the request lifecycle. There was no previous hook, the next hook will be `preParsing`.
+ *  Notice: in the `onRequest` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
  */
-export type preParsingHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = FastifyMiddleware<RawServer, RawRequest, RawReply>
-
-/**
- * preValidation hook useful for authentication. Runs after request, middleware, and parsing steps.
- */
-export type preValidationHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = FastifyMiddleware<RawServer, RawRequest, RawReply>
-
-/**
- * preSerialization hook that is triggered after a request is processed, but before the response is serialized
- */
-export type preSerializationHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = (
-  req: FastifyRequest<RawServer, RawRequest>,
-  reply: FastifyReply<RawServer, RawReply>,
-  payload: unknown,
-  done: (err?: Error, value?: any) => void
-) => FastifyMiddleware<RawServer, RawRequest, RawReply> | void
-
-/**
- * preHandler hook that is triggered before a request is processed, but after the preValidation hook
- */
-export type preHandlerHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = FastifyMiddleware<RawServer, RawRequest, RawReply>
-
-/**
- * Adds a hook that is triggered when server.close is called. Useful for closing connections
- * and performing cleanup tasks
- */
-export type onCloseHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = (instance: FastifyInstance<RawServer, RawRequest, RawReply>, done: () => void) => void
-
-/**
- * Adds a hook that is triggered when a new route is registered. Listeners are passed a
- * routeOptions object as the sole parameter.
- * The interface is synchronous, and, as such, the listeners do not get passed a callback.
- */
-export type onRouteHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = (opts: RouteOptions<RawServer, RawRequest, RawReply> & { path: string; prefix: string }) => void
-
-/**
- * Handler function for onRequest hook
- */
-export type onRequestHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = FastifyMiddleware<RawServer, RawRequest, RawReply>
-
-/**
- * Handler function for onRequest hook
- */
-export type onResponseHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = FastifyMiddleware<RawServer, RawRequest, RawReply>
-
-/**
- * Hook that is fired after a request is processed, but before the "onResponse" hook
- */
-export type onSendHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = (
-  req: FastifyRequest<RawServer, RawRequest>,
-  reply: FastifyReply<RawServer, RawReply>,
-  payload: unknown,
-  done: (err?: Error, value?: any) => void
-) => FastifyMiddleware<RawServer, RawRequest, RawReply> | void
-
-/**
- * Hook that is fired if `reply.send` is invoked with an Error
- */
-export type onErrorHookHandler<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> = (
-  req: FastifyRequest<RawServer, RawRequest>,
-  reply: FastifyReply<RawServer, RawReply>,
-  error: FastifyError,
-  done: () => void
-) => FastifyMiddleware<RawServer, RawRequest, RawReply> | void
-
-export type Hooks = 'onError' | 'onSend' | 'onRequest' | 'onResponse' | 'onRoute' | 'onClose' | 'preHandler' | 'preSerialization' | 'preValidation' | 'preParsing'
-
-export interface AddHook<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> {
-  <Name extends Hooks>(
-    name: Name,
-    hook: addHookHandler<Name, RawServer, RawRequest, RawReply>
-  ): FastifyInstance<RawServer, RawRequest, RawReply>;
-}
-
-type addHookHandler<
-  Name extends Hooks,
+export interface onRequestHookHandler<
   RawServer extends RawServerBase = RawServerDefault,
   RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
   RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
-> = Name extends 'onError' ? onErrorHookHandler<RawServer, RawRequest, RawReply>
-  : Name extends 'onSend' ? onSendHookHandler<RawServer, RawRequest, RawReply>
-    : Name extends 'onRequest' ? onRequestHookHandler<RawServer, RawRequest, RawReply>
-      : Name extends 'onResponse' ? onResponseHookHandler<RawServer, RawRequest, RawReply>
-        : Name extends 'onRoute' ? onRouteHookHandler<RawServer, RawRequest, RawReply>
-          : Name extends 'onClose' ? onCloseHookHandler<RawServer, RawRequest, RawReply>
-            : Name extends 'preHandler' ? preHandlerHookHandler<RawServer, RawRequest, RawReply>
-              : Name extends 'preSerialization' ? preSerializationHookHandler<RawServer, RawRequest, RawReply>
-                : Name extends 'preValidation' ? preValidationHookHandler<RawServer, RawRequest, RawReply>
-                  : Name extends 'preParsing' ? preParsingHookHandler<RawServer, RawRequest, RawReply>
-                    : never
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>, 
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
+
+/**
+ * `preParsing` is the second hook to be executed in the request lifecycle. The previous hook was `onRequest`, the next hook will be `preValidation`.
+ * Notice: in the `preParsing` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
+ */
+export interface preParsingHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>, 
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
+
+/**
+ * `preValidation` is the third hook to be executed in the request lifecycle. The previous hook was `preParsing`, the next hook will be `preHandler`.
+ * Notice: in the `preValidation` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
+ */
+export interface preValidationHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>, 
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
+
+/**
+ * `preHandler` is the fourth hook to be executed in the request lifecycle. The previous hook was `preValidation`, the next hook will be `preSerialization`.
+ */
+export interface preHandlerHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>, 
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
+
+// This is used within the `preSerialization` and `onSend` hook handlers
+interface DoneFuncWithErrOrRes {
+  (err: FastifyError): void;
+  (err: null, res: unknown): void;
+}
+
+/**
+ * `preSerialization` is the fifth hook to be executed in the request lifecycle. The previous hook was `preHandler`, the next hook will be `onSend`.
+ *  Note: the hook is NOT called if the payload is a string, a Buffer, a stream or null.
+ */
+export interface preSerializationHookHandler<
+  PreSerializationPayload,
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>,
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>,
+    payload: PreSerializationPayload,
+    done: DoneFuncWithErrOrRes
+  ): Promise<unknown> | void
+}
+
+/**
+ * You can change the payload with the `onSend` hook. It is the sixth hook to be executed in the request lifecycle. The previous hook was `preSerialization`, the next hook will be `onResponse`.
+ * Note: If you change the payload, you may only change it to a string, a Buffer, a stream, or null.
+ */
+export interface onSendHookHandler<
+  OnSendPayload,
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>,
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>,
+    payload: OnSendPayload,
+    done: DoneFuncWithErrOrRes
+  ): Promise<unknown> | void
+}
+
+/**
+ * `onResponse` is the seventh and last hook in the request hook lifecycle. The previous hook was `onSend`, there is no next hook.
+ * The onResponse hook is executed when a response has been sent, so you will not be able to send more data to the client. It can however be useful for sending data to external services, for example to gather statistics.
+ */
+export interface onResponseHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>, 
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
+
+/**
+ * This hook is useful if you need to do some custom error logging or add some specific header in case of error.
+ * It is not intended for changing the error, and calling reply.send will throw an exception.
+ * This hook will be executed only after the customErrorHandler has been executed, and only if the customErrorHandler sends an error back to the user (Note that the default customErrorHandler always sends the error back to the user).
+ * Notice: unlike the other hooks, pass an error to the done function is not supported.
+ */
+export interface onErrorHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> {
+  (
+    request: FastifyRequest<RawServer, RawRequest, RequestGeneric>,
+    reply: FastifyReply<RawServer, RawReply, ContextConfig>,
+    error: FastifyError,
+    done: () => void
+  ): Promise<unknown> | void
+}
+
+// Application Hooks
+
+/**
+ * Triggered when a new route is registered. Listeners are passed a routeOptions object as the sole parameter. The interface is synchronous, and, as such, the listener does not get passed a callback
+ */
+export interface onRouteHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+  ContextConfig = ContextConfigDefault
+> { 
+  (
+    opts: RouteOptions<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig> & { path: string; prefix: string }
+  ): Promise<unknown> | void
+}
+
+/**
+ * Triggered when a new plugin is registered and a new encapsulation context is created. The hook will be executed before the registered code.
+ * This hook can be useful if you are developing a plugin that needs to know when a plugin context is formed, and you want to operate in that specific context.
+ * Note: This hook will not be called if a plugin is wrapped inside fastify-plugin.
+ */
+export interface onRegisterHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  Logger = FastifyLoggerOptions<RawServer>
+> {
+  (
+    instance: FastifyInstance<RawServer, RawRequest, RawReply, Logger>, 
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void // documentation is missing the `done` method
+}
+
+/**
+ * Triggered when fastify.close() is invoked to stop the server. It is useful when plugins need a "shutdown" event, for example to close an open connection to a database.
+ */
+export interface onCloseHookHandler<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  Logger = FastifyLoggerOptions<RawServer>
+> {
+  (
+    instance: FastifyInstance<RawServer, RawRequest, RawReply, Logger>,
+    done: HookHandlerDoneFunction
+  ): Promise<unknown> | void
+}
--- types/instance.d.ts
+++ types/instance.d.ts
@@ -4,7 +4,7 @@ import { FastifySchema, FastifySchemaCompiler } from './schema'
 import { RawServerBase, RawRequestDefaultExpression, RawServerDefault, RawReplyDefaultExpression, ContextConfigDefault } from './utils'
 import { FastifyLoggerOptions } from './logger'
 import { FastifyRegister } from './register'
-import { AddHook } from './hooks'
+import { onRequestHookHandler, preParsingHookHandler, onSendHookHandler, preValidationHookHandler, preHandlerHookHandler, preSerializationHookHandler, onResponseHookHandler, onErrorHookHandler, onRouteHookHandler, onRegisterHookHandler, onCloseHookHandler } from './hooks'
 import { FastifyRequest, RequestGenericInterface } from './request'
 import { FastifyReply } from './reply'
 import { FastifyError } from './error'
@@ -74,7 +74,139 @@ export interface FastifyInstance<
   patch: RouteShorthandMethod<RawServer, RawRequest, RawReply>;
   all: RouteShorthandMethod<RawServer, RawRequest, RawReply>;
 
-  addHook: AddHook<RawServer, RawRequest, RawReply>;
+  // addHook: overloads
+
+  // Lifecycle addHooks
+
+  /**
+   * `onRequest` is the first hook to be executed in the request lifecycle. There was no previous hook, the next hook will be `preParsing`.
+   *  Notice: in the `onRequest` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'onRequest',
+    hook: onRequestHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * `preParsing` is the second hook to be executed in the request lifecycle. The previous hook was `onRequest`, the next hook will be `preValidation`.
+   * Notice: in the `preParsing` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'preParsing',
+    hook: preParsingHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * `preValidation` is the third hook to be executed in the request lifecycle. The previous hook was `preParsing`, the next hook will be `preHandler`.
+   * Notice: in the `preValidation` hook, request.body will always be null, because the body parsing happens before the `preHandler` hook.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'preValidation',
+    hook: preValidationHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * `preHandler` is the fourth hook to be executed in the request lifecycle. The previous hook was `preValidation`, the next hook will be `preSerialization`.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'preHandler',
+    hook: preHandlerHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * `preSerialization` is the fifth hook to be executed in the request lifecycle. The previous hook was `preHandler`, the next hook will be `onSend`.
+   *  Note: the hook is NOT called if the payload is a string, a Buffer, a stream or null.
+   */
+  addHook<
+    PreSerializationPayload = unknown,
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'preSerialization',
+    hook: preSerializationHookHandler<PreSerializationPayload, RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * You can change the payload with the `onSend` hook. It is the sixth hook to be executed in the request lifecycle. The previous hook was `preSerialization`, the next hook will be `onResponse`.
+   * Note: If you change the payload, you may only change it to a string, a Buffer, a stream, or null.
+   */
+  addHook<
+    OnSendPayload = unknown,
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'onSend',
+    hook: onSendHookHandler<OnSendPayload, RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * `onResponse` is the seventh and last hook in the request hook lifecycle. The previous hook was `onSend`, there is no next hook.
+   * The onResponse hook is executed when a response has been sent, so you will not be able to send more data to the client. It can however be useful for sending data to external services, for example to gather statistics.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'onResponse',
+    hook: onResponseHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  /**
+   * This hook is useful if you need to do some custom error logging or add some specific header in case of error.
+   * It is not intended for changing the error, and calling reply.send will throw an exception.
+   * This hook will be executed only after the customErrorHandler has been executed, and only if the customErrorHandler sends an error back to the user (Note that the default customErrorHandler always sends the error back to the user).
+   * Notice: unlike the other hooks, pass an error to the done function is not supported.
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'onError',
+    hook: onErrorHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+  // Application addHooks
+
+  /**
+   * Triggered when a new route is registered. Listeners are passed a routeOptions object as the sole parameter. The interface is synchronous, and, as such, the listener does not get passed a callback
+   */
+  addHook<
+    RequestGeneric extends RequestGenericInterface = RequestGenericInterface,
+    ContextConfig = ContextConfigDefault
+  >(
+    name: 'onRoute',
+    hook: onRouteHookHandler<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+   /**
+   * Triggered when a new plugin is registered and a new encapsulation context is created. The hook will be executed before the registered code.
+   * This hook can be useful if you are developing a plugin that needs to know when a plugin context is formed, and you want to operate in that specific context.
+   * Note: This hook will not be called if a plugin is wrapped inside fastify-plugin.
+   */
+  addHook(
+    name: 'onRegister',
+    hook: onRegisterHookHandler<RawServer, RawRequest, RawReply, Logger>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
+
+   /**
+   * Triggered when fastify.close() is invoked to stop the server. It is useful when plugins need a "shutdown" event, for example to close an open connection to a database.
+   */
+  addHook(
+    name: 'onClose',
+    hook: onCloseHookHandler<RawServer, RawRequest, RawReply, Logger>
+  ): FastifyInstance<RawServer, RawRequest, RawReply>;
 
   /**
    * Set the 404 handler

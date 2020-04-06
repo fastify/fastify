480a7155463555a2d3bbd94b832428c28118cd77
Thomas Heymann <thom@thoms-air.home>
2020-04-06 22:30:03 +0100

    Removed server constraints from plugin types

--- types/register.d.ts
+++ types/register.d.ts
@@ -1,13 +1,8 @@
-import { RawServerBase, RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression } from './utils'
 import { FastifyPlugin, FastifyPluginOptions } from './plugin'
 import { LogLevels } from './logger'
 
-export interface FastifyRegister<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
-> {
-  <Options = FastifyPluginOptions>(plugin: FastifyPlugin<Options, RawServer>, opts?: (RegisterOptions & Options) | (() => RegisterOptions & Options)): void;
+export interface FastifyRegister {
+  <Options extends FastifyPluginOptions>(plugin: FastifyPlugin<Options>, opts?: (RegisterOptions & Options) | (() => RegisterOptions & Options)): void;
 }
 
 export interface RegisterOptions {

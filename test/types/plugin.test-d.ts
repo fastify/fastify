c912df38ced8b006f01537039d01bd30b860dac3
Thomas Heymann <thom@thoms-air.home>
2020-04-15 12:21:29 +0100

    fix: Chainable / thenable types & after signature

--- test/types/plugin.test-d.ts
+++ test/types/plugin.test-d.ts
@@ -1,7 +1,7 @@
 import fastify, { FastifyPlugin, FastifyInstance, FastifyPluginOptions } from '../../fastify'
 import * as http from 'http'
 import * as https from 'https'
-import { expectType, expectError } from 'tsd'
+import { expectType, expectError, expectAssignable } from 'tsd'
 
 // FastifyPlugin & FastifyRegister
 interface TestOptions extends FastifyPluginOptions {
@@ -11,14 +11,30 @@ interface TestOptions extends FastifyPluginOptions {
 const testPlugin: FastifyPlugin<TestOptions> = function (instance, opts, next) { }
 
 expectError(fastify().register(testPlugin, {})) // error because missing required options from generic declaration
-expectType<void>(fastify().register(testPlugin, { option1: '', option2: true }))
-
-expectType<void>(fastify().register(function (instace, opts, next) { }))
-expectType<void>(fastify().register(function (instace, opts, next) { }, () => { }))
-expectType<void>(fastify().register(function (instance, opts, next) { }, { logLevel: 'info', prefix: 'foobar' }))
+expectAssignable<FastifyInstance>(fastify().register(testPlugin, { option1: '', option2: true }))
+expectAssignable<FastifyInstance>(fastify().register(function (instace, opts, next) { }))
+expectAssignable<FastifyInstance>(fastify().register(function (instace, opts, next) { }, () => { }))
+expectAssignable<FastifyInstance>(fastify().register(function (instance, opts, next) { }, { logLevel: 'info', prefix: 'foobar' }))
 expectError(fastify().register(function (instance, opts, next) { }, { logLevel: '' })) // must use a valid logLevel
 
 const httpsServer = fastify({ https: {} });
 expectType<FastifyInstance<https.Server, http.IncomingMessage, http.ServerResponse>>(httpsServer)
 
-httpsServer.register(testPlugin);
+// Chainable
+httpsServer
+  .register(testPlugin)
+  .after((_error) => { })
+  .close(() => { })
+  .ready((_error) => { })
+
+// Thenable
+expectAssignable<PromiseLike<undefined>>(httpsServer.after());
+expectAssignable<PromiseLike<undefined>>(httpsServer.close());
+expectAssignable<PromiseLike<undefined>>(httpsServer.ready());
+expectAssignable<PromiseLike<undefined>>(httpsServer.register(testPlugin));
+
+async function testAsync() {
+  await httpsServer
+    .register(testPlugin)
+    .register(testPlugin)
+}

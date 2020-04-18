c8fde496a17e3d0421561c39d3434fd7ff93d34c
Igor Savin <iselwin@gmail.com>
2020-04-18 22:42:34 +0200

    Add test for change in type

--- test/types/fastify.test-d.ts
+++ test/types/fastify.test-d.ts
@@ -2,6 +2,7 @@ import fastify, { FastifyInstance } from '../../fastify'
 import * as http from 'http'
 import * as https from 'https'
 import * as http2 from 'http2'
+import { Chain as LightMyRequestChain } from 'light-my-request';
 import { expectType, expectError } from 'tsd'
 
 // FastifyInstance
@@ -13,5 +14,7 @@ expectType<FastifyInstance<https.Server, http.IncomingMessage, http.ServerRespon
 // http2 server
 expectType<FastifyInstance<http2.Http2Server, http2.Http2ServerRequest, http2.Http2ServerResponse>>(fastify({ http2: true }))
 expectType<FastifyInstance<http2.Http2SecureServer, http2.Http2ServerRequest, http2.Http2ServerResponse>>(fastify({ http2: true, https: {}}))
+expectType<LightMyRequestChain>(fastify({ http2: true, https: {}}).inject())
+
 expectError(fastify<http2.Http2Server>({ http2: false })) // http2 option must be true
 expectError(fastify<http2.Http2SecureServer>({ http2: false })) // http2 option must be true

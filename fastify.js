6bd7cd9e82c826439416bc348d782261dc032700
Paolo Insogna <paolo@cowtech.it>
2020-05-08 14:21:22 +0200

    Added test case.

--- fastify.js
+++ fastify.js
@@ -390,6 +390,7 @@ function fastify (options) {
 
   function defaultClientErrorHandler (err, socket) {
     // In case of a connection reset, the socket has been destroyed and there is nothing that needs to be done.
+    // See: https://github.com/nodejs/node/issues/33302
     if (err.code === 'ECONNRESET') {
       return
     }

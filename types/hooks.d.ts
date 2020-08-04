62ad09be6f7baa81a9097770b58087821ddf67e0
Vincent LE GOFF <vince.legoff@gmail.com>
2020-08-04 10:09:31 +0200

    doc: add documentation

--- types/hooks.d.ts
+++ types/hooks.d.ts
@@ -152,6 +152,10 @@ export interface onResponseHookHandler<
   ): Promise<unknown> | void;
 }
 
+/**
+ * `onTimeout` is useful if you need to monitor the request timed out in your service. (if the `connectionTimeout` property is set on the fastify instance)
+ * The onTimeout hook is executed when a request is timed out and the http socket has been hanged up. Therefore you will not be able to send data to the client.
+ */
 export interface onTimeoutHookHandler<
   RawServer extends RawServerBase = RawServerDefault,
   RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
--- types/instance.d.ts
+++ types/instance.d.ts
@@ -167,6 +167,10 @@ export interface FastifyInstance<
     hook: onResponseHookHandler<RawServer, RawRequest, RawReply, RouteGeneric, ContextConfig>
   ): FastifyInstance<RawServer, RawRequest, RawReply, Logger>;
 
+  /**
+   * `onTimeout` is useful if you need to monitor the request timed out in your service. (if the `connectionTimeout` property is set on the fastify instance)
+   * The onTimeout hook is executed when a request is timed out and the http socket has been hanged up. Therefore you will not be able to send data to the client.
+   */
   addHook<
     RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
     ContextConfig = ContextConfigDefault

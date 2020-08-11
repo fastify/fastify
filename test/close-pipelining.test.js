1755d5e8196150fc0c8af975017bc9b28e203a55
Vincent LE GOFF <vince.legoff@gmail.com>
2020-08-11 15:13:00 +0200

    feat: switch test to undici

--- test/close-pipelining.test.js
+++ test/close-pipelining.test.js
@@ -3,41 +3,40 @@
 const t = require('tap')
 const test = t.test
 const Fastify = require('..')
-const autocannon = require('autocannon')
-
-t.jobs = 2
+const { Client } = require('undici')
 
 test('Should return 503 while closing - pipelining', t => {
-  const fastify = Fastify()
+  const fastify = Fastify({
+    return503OnClosing: true
+  })
 
   fastify.get('/', (req, reply) => {
     fastify.close()
     reply.send({ hello: 'world' })
   })
 
-  fastify.listen(0, err => {
+  fastify.listen(0, async err => {
     t.error(err)
 
-    const instance = autocannon({
-      url: 'http://localhost:' + fastify.server.address().port,
-      pipelining: 1,
-      connections: 1,
-      amount: 10
+    const instance = new Client('http://localhost:' + fastify.server.address().port,{
+      pipelining: 1
     })
 
     const codes = [200, 503]
-    instance.on('response', (client, statusCode) => {
-      t.strictEqual(statusCode, codes.shift())
-    })
-
-    instance.on('done', () => {
+    // eslint-disable-next-line no-unused-vars
+    for (const _ of Array(2)) {
+      await instance.request(
+        { path: '/', method: 'GET' }
+      ).then(data => {
+        t.strictEqual(data.statusCode, codes.shift())
+      }).catch(() => {
+        t.strictEqual(codes.shift(), undefined)
+      })
+    }
+    instance.close(() => {
       t.strictEqual(codes.length, 0)
       t.end('Done')
     })
-    instance.on('reqError', () => {
-      t.strictEqual(codes.shift(), undefined)
-    })
-    instance.on('error', err => t.fail(err))
   })
 })
 
@@ -51,28 +50,27 @@ test('Should not return 503 while closing - pipelining - return503OnClosing', t 
     reply.send({ hello: 'world' })
   })
 
-  fastify.listen(0, err => {
+  fastify.listen(0, async err => {
     t.error(err)
 
-    const instance = autocannon({
-      url: 'http://localhost:' + fastify.server.address().port,
-      pipelining: 1,
-      connections: 1,
-      amount: 10
+    const instance = new Client('http://localhost:' + fastify.server.address().port,{
+      pipelining: 1
     })
 
     const codes = [200, 200]
-    instance.on('response', (client, statusCode) => {
-      t.strictEqual(statusCode, codes.shift())
-    })
-
-    instance.on('done', () => {
+    // eslint-disable-next-line no-unused-vars
+    for (const _ of Array(2)) {
+      await instance.request(
+        { path: '/', method: 'GET' }
+      ).then(data => {
+        t.strictEqual(data.statusCode, codes.shift())
+      }).catch(() => {
+        t.strictEqual(codes.shift(), undefined)
+      })
+    }
+    instance.close(() => {
       t.strictEqual(codes.length, 0)
       t.end('Done')
     })
-    instance.on('reqError', () => {
-      t.strictEqual(codes.shift(), undefined)
-    })
-    instance.on('error', err => t.fail(err))
   })
 })

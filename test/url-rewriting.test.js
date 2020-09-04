8208eaa1375a12841ce0648c749e6d4aa404425f
Daniele Belardi <dwon.dnl@gmail.com>
2020-09-04 16:57:59 +0200

    test: 100% branch coverage on fastify.js

--- test/url-rewriting.test.js
+++ test/url-rewriting.test.js
@@ -38,6 +38,37 @@ test('Should rewrite url', t => {
   t.tearDown(() => fastify.close())
 })
 
+test('Should not rewrite if the url is the same', t => {
+  t.plan(4)
+  const fastify = Fastify({
+    rewriteUrl (req) {
+      t.equal(req.url, '/this-would-404-without-url-rewrite')
+      return req.url
+    }
+  })
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: (req, reply) => {
+      reply.send({ hello: 'world' })
+    }
+  })
+
+  fastify.listen(0, function (err) {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/this-would-404-without-url-rewrite'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 404)
+    })
+  })
+
+  t.tearDown(() => fastify.close())
+})
 test('Should throw an error', t => {
   t.plan(5)
   const fastify = Fastify({

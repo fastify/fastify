208e3b20e9c9e06672a76f494b661e580067b310
Abtin Gramian <abtin.gramian@gmail.com>
2020-09-15 10:08:58 -0700

    Copy type definition for PrettyOptions from pino into fastify logger

--- types/logger.d.ts
+++ types/logger.d.ts
@@ -1,7 +1,6 @@
 import { FastifyError } from 'fastify-error'
 import { RawServerBase, RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression } from './utils'
 import { FastifyRequest, RequestGenericInterface } from './request'
-import { PrettyOptions } from 'pino'
 
 /**
  * Standard Fastify logging function
@@ -29,6 +28,61 @@ export interface FastifyLoggerInstance {
   trace: FastifyLogFn;
   debug: FastifyLogFn;
   child(bindings: Bindings): FastifyLoggerInstance;
+}
+
+export interface PrettyOptions {
+  /**
+   * Translate the epoch time value into a human readable date and time string.
+   * This flag also can set the format string to apply when translating the date to human readable format.
+   * The default format is yyyy-mm-dd HH:MM:ss.l o in UTC.
+   * For a list of available pattern letters see the {@link https://www.npmjs.com/package/dateformat|dateformat documentation}.
+   */
+  translateTime?: boolean | string;
+  /**
+   * If set to true, it will print the name of the log level as the first field in the log line. Default: `false`.
+   */
+  levelFirst?: boolean;
+  /**
+   * The key in the JSON object to use as the highlighted message. Default: "msg".
+   */
+  messageKey?: string;
+  /**
+   * The key in the JSON object to use for timestamp display. Default: "time".
+   */
+  timestampKey?: string;
+  /**
+   * Format output of message, e.g. {level} - {pid} will output message: INFO - 1123 Default: `false`.
+   */
+  messageFormat?: false | string;
+  /**
+   * If set to true, will add color information to the formatted output message. Default: `false`.
+   */
+  colorize?: boolean;
+  /**
+   * Appends carriage return and line feed, instead of just a line feed, to the formatted log line.
+   */
+  crlf?: boolean;
+  /**
+   * Define the log keys that are associated with error like objects. Default: ["err", "error"]
+   */
+  errorLikeObjectKeys?: string[];
+  /**
+   *  When formatting an error object, display this list of properties.
+   *  The list should be a comma separated list of properties. Default: ''
+   */
+  errorProps?: string;
+  /**
+   * Specify a search pattern according to {@link http://jmespath.org|jmespath}
+   */
+  search?: string;
+  /**
+   * Ignore one or several keys. Example: "time,hostname"
+   */
+  ignore?: string;
+  /**
+   * Suppress warning on first synchronous flushing.
+   */
+  suppressFlushSyncWarning?: boolean;
 }
 
 /**

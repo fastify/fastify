04e06c4d9ab7cd40c8e8236d10a633b5c8afbf3a
Matteo Collina <hello@matteocollina.com>
2020-09-30 12:49:36 +0200

    Support for named export in Node v14.13.0

---
+++ examples/simple.mjs
@@ -0,0 +1,27 @@
+// works on Node v14.13.0+
+import { fastify } from '../fastify.js'
+
+const app = fastify({
+  logger: false
+})
+
+const schema = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
+
+app.get('/', schema, async function (req, reply) {
+  return { hello: 'world' }
+})
+
+app.listen(3000).catch(console.error)

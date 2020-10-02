8f1d30b6d47d98b7e0d34fb5c4dcf627cf6dce22
Mitul Shah <45.mitul@gmail.com>
2020-10-02 15:35:03 +0200

    Reverting `export *` syntax (#2597)
    Reverting `export *` syntax because it is breaking module augmentation for `FastifyReply`
    
    For example, the following code fails:
    
    ```
    import fastify from 'fastify';
    
    declare module 'fastify' {
      interface FastifyReply {
        hello: string;
      }
    }
    
    const server = fastfiy();
    server.decorateReply('hello', 'world');
    
    server.get('/hello', async (request, reply) => {
      reply.send(reply.hello);
    })
    ```
    with error: `Property 'hello' does not exist on type 'FastifyReply<Server, IncomingMessage, ServerResponse, RouteGenericInterface, unknown>'.`
    
    And after applying the fix, the code works as expected and returns `world`.

--- fastify.d.ts
+++ fastify.d.ts
@@ -119,8 +119,8 @@ export type FastifyServerOptions<
 type TrustProxyFunction = (address: string, hop: number) => boolean
 
 /* Export all additional types */
-export * from './types/request'
-export * from './types/reply'
+export { FastifyRequest, RequestGenericInterface } from './types/request'
+export { FastifyReply } from './types/reply'
 export { FastifyPluginCallback, FastifyPluginAsync, FastifyPluginOptions, FastifyPlugin } from './types/plugin'
 export { FastifyInstance } from './types/instance'
 export { FastifyLoggerOptions, FastifyLoggerInstance, FastifyLogFn, LogLevel } from './types/logger'

133ae363bc4438ae43424649610495d56b08ccbc
Vincent LE GOFF <vince.legoff@gmail.com>
2020-10-08 00:16:53 +0200

    fix: add test

--- test/internals/version.test.js
+++ test/internals/version.test.js
@@ -4,9 +4,16 @@ const t = require('tap')
 const test = t.test
 const proxyquire = require('proxyquire')
 const Fastify = proxyquire('../..', { fs: { accessSync: () => { throw Error('error') } } })
+const Fastify2 = proxyquire('../..', { fs: { accessSync: () => { }, readFileSync: () => JSON.stringify({ name: 'foo', version: '6.6.6' }) } })
 
 test('should output an undefined version in case of package.json not available', t => {
   t.plan(1)
   const srv = Fastify()
+  t.is(srv.version, undefined)
+})
+
+test('should output an undefined version in case of package.json is not the fastify one', t => {
+  t.plan(1)
+  const srv = Fastify2()
   t.is(srv.version, undefined)
 })

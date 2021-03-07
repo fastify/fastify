6e122a0728237e1a8e798e7f53a69b4864b9d91d
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-07 15:59:45 +0200

    fixed main

--- tools/website.sh
+++ tools/website.sh
@@ -2,6 +2,6 @@
 
 NODE_VER=`node -v`
 
-if [[ $NODE_VER =~ ^v6 && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "main" ]]; then
+if [[ $NODE_VER =~ ^v6 && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "HEAD" ]]; then
   curl -X POST --header 'Content-Type: application/json' "https://circleci.com/api/v1.1/project/github/fastify/website?circle-token=$CIRCLECI_TOKEN"
 fi

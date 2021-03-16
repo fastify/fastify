53e3e792ebd5aba68be94f753f48d2e9b2ed832c
Manuel Spigolon <behemoth89@gmail.com>
2021-03-16 17:12:48 +0100

    fix regression: schema duplicate (#2925)
    * build(deps-dev): bump concurrently from 5.3.0 to 6.0.0 (#62)
    
    Bumps [concurrently](https://github.com/kimmobrunfeldt/concurrently) from 5.3.0 to 6.0.0.
    - [Release notes](https://github.com/kimmobrunfeldt/concurrently/releases)
    - [Commits](https://github.com/kimmobrunfeldt/concurrently/compare/v5.3.0...v6.0.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build(deps): bump fastify/github-action-merge-dependabot (#65)
    
    Bumps [fastify/github-action-merge-dependabot](https://github.com/fastify/github-action-merge-dependabot) from v1.1.1 to v1.2.1.
    - [Release notes](https://github.com/fastify/github-action-merge-dependabot/releases)
    - [Commits](https://github.com/fastify/github-action-merge-dependabot/compare/v1.1.1...afbe82ba990ac7da327bdc087b27ad62023100dc)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: ajv creation when not necessary
    
    * test: add regression test
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/schema-special-usage.test.js
+++ test/schema-special-usage.test.js
@@ -2,6 +2,7 @@
 
 const { test } = require('tap')
 const AJV = require('ajv')
+const S = require('fluent-json-schema')
 const Fastify = require('..')
 const ajvMergePatch = require('ajv-merge-patch')
 
@@ -400,6 +401,57 @@ test('side effect on schema let the server crash', async t => {
       response: { 200: reusedSchema }
     }
   })
+
+  await fastify.ready()
+})
+
+test('only response schema trigger AJV pollution', async t => {
+  const ShowSchema = S.object().id('ShowSchema').prop('name', S.string())
+  const ListSchema = S.array().id('ListSchema').items(S.ref('ShowSchema#'))
+
+  const fastify = Fastify()
+  fastify.addSchema(ListSchema)
+  fastify.addSchema(ShowSchema)
+
+  const routeResponseSchemas = {
+    schema: { response: { 200: S.ref('ListSchema#') } }
+  }
+
+  fastify.register(
+    async (app) => { app.get('/resource/', routeResponseSchemas, () => ({})) },
+    { prefix: '/prefix1' }
+  )
+  fastify.register(
+    async (app) => { app.get('/resource/', routeResponseSchemas, () => ({})) },
+    { prefix: '/prefix2' }
+  )
+
+  await fastify.ready()
+})
+
+test('only response schema trigger AJV pollution #2', async t => {
+  const ShowSchema = S.object().id('ShowSchema').prop('name', S.string())
+  const ListSchema = S.array().id('ListSchema').items(S.ref('ShowSchema#'))
+
+  const fastify = Fastify()
+  fastify.addSchema(ListSchema)
+  fastify.addSchema(ShowSchema)
+
+  const routeResponseSchemas = {
+    schema: {
+      params: S.ref('ListSchema#'),
+      response: { 200: S.ref('ListSchema#') }
+    }
+  }
+
+  fastify.register(
+    async (app) => { app.get('/resource/', routeResponseSchemas, () => ({})) },
+    { prefix: '/prefix1' }
+  )
+  fastify.register(
+    async (app) => { app.get('/resource/', routeResponseSchemas, () => ({})) },
+    { prefix: '/prefix2' }
+  )
 
   await fastify.ready()
 })

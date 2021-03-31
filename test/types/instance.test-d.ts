5ab8fb4a0f7eda33d9d6d813409bd892d379f29b
matthyk <53833818+matthyk@users.noreply.github.com>
2021-03-31 16:30:20 +0200

    fix types (#2961)

--- test/types/instance.test-d.ts
+++ test/types/instance.test-d.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyContentTypeParser, FastifyError, FastifyInstance, ValidationResult } from '../../fastify'
+import fastify, { FastifyBodyParser, FastifyError, FastifyInstance, ValidationResult } from '../../fastify'
 import { expectAssignable, expectError, expectType } from 'tsd'
 import { FastifyRequest } from '../../types/request'
 import { FastifyReply } from '../../types/reply'
@@ -123,6 +123,6 @@ type InitialConfig = Readonly<{
 
 expectType<InitialConfig>(fastify().initialConfig)
 
-expectType<FastifyContentTypeParser>(server.defaultTextParser)
+expectType<FastifyBodyParser<string>>(server.defaultTextParser)
 
-expectType<FastifyContentTypeParser>(server.getDefaultJsonParser('ignore', 'error'))
+expectType<FastifyBodyParser<string>>(server.getDefaultJsonParser('ignore', 'error'))

5deae9d367010669c3fcaed161671b1ab8b0604b
Matteo Collina <hello@matteocollina.com>
2021-05-29 10:28:00 +0200

    Revert "fixup! Simplify reply sent monitoring (#3072) (#3079)"
    This reverts commit 9bf6a332d32b11e2fb05d18ef26b85b5a060f627.

--- test/internals/handleRequest.test.js
+++ test/internals/handleRequest.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { test } = require('tap')
+const semver = require('semver')
 const handleRequest = require('../../lib/handleRequest')
 const internals = require('../../lib/handleRequest')[Symbol.for('internals')]
 const Request = require('../../lib/request')
@@ -107,8 +108,12 @@ test('handler function - preValidationCallback with finished response', t => {
   t.plan(0)
   const res = {}
   // Be sure to check only `writableEnded` where is available
-  res.writableEnded = true
-
+  if (semver.gte(process.versions.node, '12.9.0')) {
+    res.writableEnded = true
+  } else {
+    res.writable = false
+    res.finished = true
+  }
   res.end = () => {
     t.fail()
   }
@@ -133,7 +138,7 @@ test('handler function - preValidationCallback with finished response (< v12.9.0
   t.plan(0)
   const res = {}
   // Be sure to check only `writableEnded` where is available
-  res.headersSent = true
+  res.writable = false
   res.finished = true
 
   res.end = () => {

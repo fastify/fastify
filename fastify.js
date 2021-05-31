9a13502237022db6a731795bbc548af0e8b907da
Vincent LE GOFF <vince.legoff@gmail.com>
2021-05-31 16:25:18 +0200

    fix: #3110 version resolution (#3113)
    * fix: #3110 version resolution

--- fastify.js
+++ fastify.js
@@ -4,9 +4,8 @@ const Avvio = require('avvio')
 const http = require('http')
 const querystring = require('querystring')
 let lightMyRequest
-let version
-let versionLoaded = false
 
+const { version } = require('./package.json')
 const {
   kAvvioBoot,
   kChildren,
@@ -309,9 +308,6 @@ function fastify (options) {
     },
     version: {
       get () {
-        if (versionLoaded === false) {
-          version = loadVersion()
-        }
         return version
       }
     },
@@ -652,20 +648,6 @@ function wrapRouting (httpHandler, { rewriteUrl, logger }) {
       }
     }
     httpHandler(req, res)
-  }
-}
-
-function loadVersion () {
-  versionLoaded = true
-  const fs = require('fs')
-  const path = require('path')
-  try {
-    const pkgPath = path.join(__dirname, 'package.json')
-    fs.accessSync(pkgPath, fs.constants.R_OK)
-    const pkg = JSON.parse(fs.readFileSync(pkgPath))
-    return pkg.name === 'fastify' ? pkg.version : undefined
-  } catch (e) {
-    return undefined
   }
 }
 

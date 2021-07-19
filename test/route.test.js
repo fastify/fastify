014d6d6e368496b9ca850d60ae66b18df50fe21a
Manuel Spigolon <behemoth89@gmail.com>
2021-07-19 15:11:26 +0200

    docs: request.context and reply.context (#3201)
    * add getter
    
    * Revert "add getter"
    
    This reverts commit 877b37eb5b52589836dfc2e7cae4c28fecf2b782.
    
    * add context docs

--- test/route.test.js
+++ test/route.test.js
@@ -1191,6 +1191,32 @@ test('HEAD routes properly auto created for GET routes when prefixTrailingSlash:
   t.equal(noneTrailingReply.statusCode, 200)
 })
 
+test('Request and Reply share the route config', async t => {
+  t.plan(3)
+
+  const fastify = Fastify()
+
+  const config = {
+    this: 'is a string',
+    thisIs: function aFunction () {}
+  }
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    config,
+    handler: (req, reply) => {
+      t.same(req.context, reply.context)
+      t.same(req.context.config, reply.context.config)
+      t.match(req.context.config, config, 'there are url and method additional properties')
+
+      reply.send({ hello: 'world' })
+    }
+  })
+
+  await fastify.inject('/')
+})
+
 test('Will not try to re-createprefixed HEAD route if it already exists and exposeHeadRoutes is true', async (t) => {
   t.plan(1)
 

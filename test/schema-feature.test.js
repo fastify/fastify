efbea2f0d3799dc67fb9f8fe0842023c5fcf7e65
Igor Savin <iselwin@gmail.com>
2021-09-08 21:03:48 +0300

    Update to pino rc4 (#3302)

--- test/schema-feature.test.js
+++ test/schema-feature.test.js
@@ -205,7 +205,12 @@ test('Should throw of the schema does not exists in output', t => {
 
   fastify.ready(err => {
     t.equal(err.code, 'FST_ERR_SCH_SERIALIZATION_BUILD')
-    t.equal(err.message, "Failed building the serialization schema for GET: /:id, due to error Cannot read property 'type' of undefined") // error from fast-json-strinfigy
+    // error from fast-json-stringify, different between Node versions
+    const isExpectedMessage = (err.message === 'Failed building the serialization schema for GET: /:id, due to error Cannot read property \'type\' of undefined' || err.message === 'Failed building the serialization schema for GET: /:id, due to error Cannot read properties of undefined (reading \'type\')')
+    if (!isExpectedMessage) {
+      console.error(`Actual error: ${err.message}`)
+    }
+    t.equal(isExpectedMessage, true)
   })
 })
 

26d29738eaacc6fe62b10415a67a114f05682ac1
KaKa <kaka@kakawebsitedemo.com>
2021-09-23 17:43:01 +0800

    refactor: remove error

--- test/maxRequestsPerSocket.test.js
+++ test/maxRequestsPerSocket.test.js
@@ -4,9 +4,10 @@ const net = require('net')
 const { test } = require('tap')
 const semver = require('semver')
 const Fastify = require('../fastify')
-const proxyquire = require('proxyquire')
 
-test('maxRequestsPerSocket on node version >= 16.10.0', { skip: semver.lt(process.versions.node, '16.10.0') }, t => {
+const skip = semver.lt(process.versions.node, '16.10.0')
+
+test('maxRequestsPerSocket on node version >= 16.10.0', { skip }, t => {
   t.plan(8)
 
   const fastify = Fastify({ maxRequestsPerSocket: 2 })
@@ -46,7 +47,7 @@ test('maxRequestsPerSocket on node version >= 16.10.0', { skip: semver.lt(proces
   })
 })
 
-test('maxRequestsPerSocket zero should behave same as null', { skip: semver.lt(process.versions.node, '16.10.0') }, t => {
+test('maxRequestsPerSocket zero should behave same as null', { skip }, t => {
   t.plan(10)
 
   const fastify = Fastify({ maxRequestsPerSocket: 0 })
@@ -88,73 +89,16 @@ test('maxRequestsPerSocket zero should behave same as null', { skip: semver.lt(p
   })
 })
 
-test('maxRequestsPerSocket should allowed on node >= 16.10.0', async (t) => {
+test('maxRequestsPerSocket should be set', async (t) => {
   t.plan(1)
-  const server = proxyquire('../lib/server', {
-    process: {
-      versions: {
-        node: '16.10.0'
-      }
-    }
-  })
-  const Fastify = proxyquire('../fastify', {
-    './lib/server.js': server
-  })
 
   const initialConfig = Fastify({ maxRequestsPerSocket: 5 }).initialConfig
   t.same(initialConfig.maxRequestsPerSocket, 5)
 })
 
-test('maxRequestsPerSocket should allowed on node >= 16.10.0', async (t) => {
+test('maxRequestsPerSocket should 0', async (t) => {
   t.plan(1)
-  const server = proxyquire('../lib/server', {
-    process: {
-      versions: {
-        node: '16.10.0'
-      }
-    }
-  })
-  const Fastify = proxyquire('../fastify', {
-    './lib/server.js': server
-  })
 
   const initialConfig = Fastify().initialConfig
   t.same(initialConfig.maxRequestsPerSocket, 0)
-})
-
-test('maxRequestsPerSocket should throw on node < 16.10.0', async (t) => {
-  t.plan(2)
-  const server = proxyquire('../lib/server', {
-    process: {
-      versions: {
-        node: '16.9.1'
-      }
-    }
-  })
-  const Fastify = proxyquire('../fastify', {
-    './lib/server.js': server
-  })
-
-  try {
-    Fastify({ maxRequestsPerSocket: 5 })
-  } catch (err) {
-    t.same(err.code, 'FST_ERR_HTTP_MAX_REQUESTS_PER_SOCKET_INVALID_VERSION')
-    t.same(err.message, '"maxRequestsPerSocket" is available only from node >= 16.10.0')
-  }
-})
-
-test('maxRequestsPerSocket should not throw when default option applied for node < 16.10.0', async (t) => {
-  const server = proxyquire('../lib/server', {
-    process: {
-      versions: {
-        node: '16.9.1'
-      }
-    }
-  })
-  const Fastify = proxyquire('../fastify', {
-    './lib/server.js': server
-  })
-
-  Fastify({ maxRequestsPerSocket: 0 })
-  t.pass()
 })

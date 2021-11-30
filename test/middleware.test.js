bb691e5d831aef6adcbe47698be3ef06abc15fc0
Rishit Pandey <rishpandey8097@gmail.com>
2021-11-30 17:10:28 +0530

    remove app.use and fix middleware tests

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -3,21 +3,8 @@
 const { test } = require('tap')
 const Fastify = require('..')
 const {
-  FST_ERR_DEC_ALREADY_PRESENT,
-  FST_ERR_MISSING_MIDDLEWARE
+  FST_ERR_DEC_ALREADY_PRESENT
 } = require('../lib/errors')
-
-test('Should throw if the basic use API has not been overridden', t => {
-  t.plan(1)
-  const fastify = Fastify()
-
-  try {
-    fastify.use()
-    t.fail('Should throw')
-  } catch (err) {
-    t.ok(err instanceof FST_ERR_MISSING_MIDDLEWARE)
-  }
-})
 
 test('Should be able to override the default use API', t => {
   t.plan(1)
@@ -38,22 +25,12 @@ test('Cannot decorate use twice', t => {
 })
 
 test('Encapsulation works', t => {
-  t.plan(2)
+  t.plan(1)
   const fastify = Fastify()
 
   fastify.register((instance, opts, done) => {
     instance.decorate('use', () => true)
     t.equal(instance.use(), true)
-    done()
-  })
-
-  fastify.register((instance, opts, done) => {
-    try {
-      instance.use()
-      t.fail('Should throw')
-    } catch (err) {
-      t.ok(err instanceof FST_ERR_MISSING_MIDDLEWARE)
-    }
     done()
   })
 

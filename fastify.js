efd34f0bba6efc693f0b4f20ab23a5a329657326
Manuel Spigolon <manuel.spigolon@nearform.com>
2022-01-05 17:21:25 +0100

    feat: collapsed preParsing hook into onRequest

--- fastify.js
+++ fastify.js
@@ -514,10 +514,8 @@ function fastify (options) {
       this[kHooks].validate(name, fn)
       this.onClose(fn)
     } else if (name === 'onReady') {
-      this[kHooks].validate(name, fn)
       this[kHooks].add(name, fn)
     } else if (name === 'onRoute') {
-      this[kHooks].validate(name, fn)
       this[kHooks].add(name, fn)
     } else {
       this.after((err, done) => {

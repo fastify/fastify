1fc6610a1793726b3cbea3358fbd687684de1818
Pietro Verdile <pietro.verdile@visto.global>
2022-01-06 13:30:57 -0300

    support ajv plugin with options

--- fastify.d.ts
+++ fastify.d.ts
@@ -144,7 +144,7 @@ export type FastifyServerOptions<
   return503OnClosing?: boolean,
   ajv?: {
     customOptions?: AjvOptions,
-    plugins?: Function[]
+    plugins?: (Function | [Function, string | string[]])[]
   },
   frameworkErrors?: <RequestGeneric extends RequestGenericInterface = RequestGenericInterface>(
     error: FastifyError,

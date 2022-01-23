a1f959975983fd400898131ab10fe7cace11b7b0
Manuel Spigolon <manuel.spigolon@nearform.com>
2022-01-23 11:12:32 +0100

    fix coverage

--- test/internals/server.test.js
+++ test/internals/server.test.js
@@ -48,3 +48,41 @@ test('DNS errors does not stop the main server on localhost - callback interface
     t.pass('server started')
   })
 })
+
+test('DNS returns empty binding', t => {
+  t.plan(2)
+  const createServer = proxyquire('../../lib/server', {
+    dns: {
+      lookup: (hostname, options, cb) => {
+        cb(null, [])
+      }
+    }
+  })
+  const { server, listen } = createServer({}, handler)
+  listen.call(Fastify(), 0, 'localhost', (err) => {
+    t.error(err)
+    server.close()
+    t.pass('server started')
+  })
+})
+
+test('DNS returns more than two binding', t => {
+  t.plan(2)
+  const createServer = proxyquire('../../lib/server', {
+    dns: {
+      lookup: (hostname, options, cb) => {
+        cb(null, [
+          { address: '::1', family: 6 },
+          { address: '127.0.0.1', family: 4 },
+          { address: '0.0.0.0', family: 4 }
+        ])
+      }
+    }
+  })
+  const { server, listen } = createServer({}, handler)
+  listen.call(Fastify(), 0, 'localhost', (err) => {
+    t.error(err)
+    server.close()
+    t.pass('server started')
+  })
+})

f427c934e4f637b58929a88ded7cdee38a28cb7c
KaKa <kaka@kakawebsitedemo.com>
2022-03-25 09:25:54 +0800

    refactor: block invalid trailers and reply te only

--- test/reply-trailers.test.js
+++ test/reply-trailers.test.js
@@ -5,6 +5,30 @@ const test = t.test
 const Fastify = require('..')
 const { Readable } = require('stream')
 
+test('do not send trailers when request do not contain correct te', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.get('/', function (request, reply) {
+    reply.addTrailer('ETag', function (reply, payload) {
+      t.fail('trailer should not be called.')
+      return 'custom-etag'
+    })
+    reply.send(JSON.stringify({ hello: 'world' }))
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/'
+  }, (error, res) => {
+    t.error(error)
+    t.equal(res.statusCode, 200)
+    t.notOk(res.headers.trailer)
+    t.notOk(res.trailers.etag)
+  })
+})
+
 test('do not send trailers when payload is empty', t => {
   t.plan(4)
 
@@ -20,7 +44,10 @@ test('do not send trailers when payload is empty', t => {
 
   fastify.inject({
     method: 'GET',
-    url: '/'
+    url: '/',
+    headers: {
+      TE: 'trailers'
+    }
   }, (error, res) => {
     t.error(error)
     t.equal(res.statusCode, 200)
@@ -45,12 +72,15 @@ test('send trailers when payload is json', t => {
 
   fastify.inject({
     method: 'GET',
-    url: '/'
+    url: '/',
+    headers: {
+      TE: 'trailers'
+    }
   }, (error, res) => {
     t.error(error)
     t.equal(res.statusCode, 200)
     t.equal(res.headers['transfer-encoding'], 'chunked')
-    t.equal(res.headers.trailer, 'ETag')
+    t.equal(res.headers.trailer, 'etag')
     t.equal(res.trailers.etag, 'custom-etag')
   })
 })
@@ -71,12 +101,57 @@ test('send trailers when payload is stream', t => {
 
   fastify.inject({
     method: 'GET',
-    url: '/'
+    url: '/',
+    headers: {
+      TE: 'trailers'
+    }
   }, (error, res) => {
     t.error(error)
     t.equal(res.statusCode, 200)
     t.equal(res.headers['transfer-encoding'], 'chunked')
-    t.equal(res.headers.trailer, 'ETag')
+    t.equal(res.headers.trailer, 'etag')
     t.equal(res.trailers.etag, 'custom-etag')
+  })
+})
+
+test('throw error when trailer header name is not allowed', t => {
+  const INVALID_TRAILERS = [
+    'transfer-encoding',
+    'content-length',
+    'host',
+    'cache-control',
+    'max-forwards',
+    'te',
+    'authorization',
+    'set-cookie',
+    'content-encoding',
+    'content-type',
+    'content-range',
+    'trailer'
+  ]
+  t.plan(INVALID_TRAILERS.length + 2)
+
+  const fastify = Fastify()
+
+  fastify.get('/', function (request, reply) {
+    for (const key of INVALID_TRAILERS) {
+      try {
+        reply.addTrailer(key, () => {})
+      } catch (err) {
+        t.equal(err.message, `Called reply.addTrailer with an invalid header name: ${key}`)
+      }
+    }
+    reply.send('')
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      TE: 'trailers'
+    }
+  }, (error, res) => {
+    t.error(error)
+    t.equal(res.statusCode, 200)
   })
 })

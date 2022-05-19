0a6c606d3cd987f6d1d84ca437ca0f68fde355ea
KaKa <kaka@kakawebsitedemo.com>
2022-05-19 16:54:47 +0800

    test: update test

--- test/internals/server.test.js
+++ test/internals/server.test.js
@@ -4,7 +4,7 @@ const { test } = require('tap')
 const proxyquire = require('proxyquire')
 
 const Fastify = require('../../fastify')
-const createServer = require('../../lib/server')
+const { createServer } = require('../../lib/server')
 
 const handler = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' })
@@ -19,7 +19,7 @@ test('start listening', async t => {
 })
 
 test('DNS errors does not stop the main server on localhost - promise interface', async t => {
-  const createServer = proxyquire('../../lib/server', {
+  const { createServer } = proxyquire('../../lib/server', {
     dns: {
       lookup: (hostname, options, cb) => {
         cb(new Error('DNS error'))
@@ -34,7 +34,7 @@ test('DNS errors does not stop the main server on localhost - promise interface'
 
 test('DNS errors does not stop the main server on localhost - callback interface', t => {
   t.plan(2)
-  const createServer = proxyquire('../../lib/server', {
+  const { createServer } = proxyquire('../../lib/server', {
     dns: {
       lookup: (hostname, options, cb) => {
         cb(new Error('DNS error'))
@@ -51,7 +51,7 @@ test('DNS errors does not stop the main server on localhost - callback interface
 
 test('DNS returns empty binding', t => {
   t.plan(2)
-  const createServer = proxyquire('../../lib/server', {
+  const { createServer } = proxyquire('../../lib/server', {
     dns: {
       lookup: (hostname, options, cb) => {
         cb(null, [])
@@ -68,7 +68,7 @@ test('DNS returns empty binding', t => {
 
 test('DNS returns more than two binding', t => {
   t.plan(2)
-  const createServer = proxyquire('../../lib/server', {
+  const { createServer } = proxyquire('../../lib/server', {
     dns: {
       lookup: (hostname, options, cb) => {
         cb(null, [

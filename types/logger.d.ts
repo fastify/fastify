f08ff6adde6f1ef1f494352e921c62730d540a94
Григорьев Сергей Николаевич <s.n.grigorev@tinkoff.ru>
2022-07-25 09:01:20 +0300

    chore(logger): add comment todo about deleting FastifyLoggerInstance type in the next major release

--- types/logger.d.ts
+++ types/logger.d.ts
@@ -23,6 +23,10 @@ export type FastifyBaseLogger = pino.BaseLogger & {
   child(bindings: Bindings, options?: ChildLoggerOptions): FastifyBaseLogger
 }
 
+// TODO delete FastifyLoggerInstance in the next major release. It seems that it is enough to have only FastifyBaseLogger.
+/**
+ * @deprecated Use FastifyBaseLogger instead
+ */
 export type FastifyLoggerInstance = FastifyBaseLogger
 
 export interface FastifyLoggerStreamDestination {

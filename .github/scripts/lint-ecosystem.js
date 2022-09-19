d449d8e0d955697a4efc1d9b97561d7694eba0d5
Zac Rosenbauer <zacrosenbauer@gmail.com>
2022-09-18 19:05:36 -0500

    Fix issues, add annotiotations + summary

--- .github/scripts/lint-ecosystem.js
+++ .github/scripts/lint-ecosystem.js
@@ -4,28 +4,41 @@ const path = require('path')
 const fs = require('fs')
 const readline = require('readline')
 
-const ecosystemDocFile = path.join(__dirname, '..', '..', 'docs', 'Guides', 'Ecosystem.md')
+const basePathEcosystemDocFile = path.join('docs', 'Guides', 'Ecosystem.md')
+const ecosystemDocFile = path.join(__dirname, '..', '..', basePathEcosystemDocFile)
+const failureTypes = {
+  improperFormat: 'improperFormat',
+  outOfOrderItem: 'outOfOrderItem'
+}
 
-module.exports = async function ({ core, github, context }) {
+module.exports = async function ({ core }) {
+  const results = await runCheck()
+  await handleResults({ core }, results)
+}
+
+async function runCheck () {
   const stream = await fs.createReadStream(ecosystemDocFile)
   const rl = readline.createInterface({
     input: stream,
     crlfDelay: Infinity
   })
 
+  const failures = []
+  const successes = []
   const moduleNameRegex = /^\- \[\`(.+)\`\]/
-  let hasOutOfOrderItem = false
   let lineNumber = 0
   let modules = []
-  let hasImproperFormat = false
+  let grouping = 'core'
 
   for await (const line of rl) {
     lineNumber += 1
     if (line.startsWith('#### [Community]')) {
+      grouping = 'community'
       modules = []
     }
 
     if (line.startsWith('#### [Community Tools]')) {
+      grouping = 'community-tools'
       modules = []
     }
 
@@ -36,56 +49,85 @@ module.exports = async function ({ core, github, context }) {
     const moduleNameTest = moduleNameRegex.exec(line)
 
     if (moduleNameTest === null) {
-      core.error(`line ${lineNumber}: improper pattern, module name should be enclosed with backticks`)
-      hasImproperFormat = true
+      failures.push({
+        lineNumber,
+        grouping,
+        moduleName: 'unknown',
+        type: failureTypes.improperFormat
+      })
       continue
     }
 
     const moduleName = moduleNameTest[1]
     if (modules.length > 0) {
       if (compare(moduleName, modules.at(-1)) > 0) {
-        core.error(`line ${lineNumber}: ${moduleName} not listed in alphabetical order`)
-        hasOutOfOrderItem = true
+        failures.push({
+          lineNumber,
+          moduleName,
+          grouping,
+          type: failureTypes.outOfOrderItem
+        })
+      } else {
+        successes.push({ moduleName, lineNumber, grouping })
       }
+    } else {
+      // We have to push the first item found or we are missing items from the list
+      successes.push({ moduleName, lineNumber, grouping })
     }
     modules.push(moduleName)
   }
 
-  const failures = []
-  if (hasOutOfOrderItem === true) {
-    const failure = 'Some ecosystem modules are not in alphabetical order.'
-    failures.push(failure)
-    core.setFailed(failure)
-  }
-
-  if (hasImproperFormat === true) {
-    const failure = 'Some ecosystem modules are improperly formatted.'
-    failures.push(failure)
-    core.setFailed(failure)
-  }
-
-  handleFailures(failures, { core, github, context })
+  return { failures, successes }
 }
 
-function handleFailures (failures, workflow) {
-  const { github, context, core } = workflow;
+async function handleResults (scriptLibs, results) {
+  const { core } = scriptLibs
+  const { failures, successes } = results;
+  const isError = !!failures.length;
 
-  if (failures.length) {
+  await core.summary
+    .addHeading(isError ? `❌ Ecosystem.md Lint (${failures.length} error${failures.length === 1 ? '' : 's' })` : '✅ Ecosystem Lint (no errors found)')
+    .addTable([
+      [
+        { data: 'Status', header: true },
+        { data: 'Section', header: true },
+        { data: 'Module', header: true },
+        { data: 'Details', header: true }],
+      ...failures.map((failure) => [
+        '❌',
+        failure.grouping,
+        failure.moduleName,
+        `Line Number: ${failure.lineNumber.toString()} - ${failure.type}`
+      ]),
+      ...successes.map((success) => [
+        '✅',
+        success.grouping,
+        success.moduleName,
+        '-'
+      ])
+    ])
+    .write()
 
-    const output = `
-    ## :x: Failed: Lint Ecosystem Order
-
-    ${failures.map((failure) => '- ' + failure + '\n')} 
-
-    [View Details](https://github.com/fastify/fastify/actions/runs/${context.runId}/jobs/${context.job})
-    `
-
-    github.rest.issues.createComment({
-      issue_number: context.issue.number,
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      body: output
+  if (isError) {
+    failures.forEach((failure) => {
+      if (failure.type === failureTypes.improperFormat) {
+        core.error('The module name should be enclosed with backticks', {
+          title: 'Improper format',
+          file: basePathEcosystemDocFile,
+          startLine: failure.lineNumber
+        })
+      } else if (failure.type === failureTypes.outOfOrderItem) {
+        core.error(`${failure.moduleName} not listed in alphabetical order`, {
+          title: 'Out of Order',
+          file: basePathEcosystemDocFile,
+          startLine: failure.lineNumber
+        })
+      } else {
+        core.error('Unknown error')
+      }
     })
+
+    core.setFailed('Failed when linting Ecosystem.md')
   }
 }
 

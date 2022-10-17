d6e05434ad77e9fc41c4815395130a1d759a06ae
jhhom <maxosen64@gmail.com>
2022-10-17 14:40:42 +0800

    fix: throws an error when registering invalid hook functions (#4332)
    * validate registered hook functions
    
    * move hooks validation to after onRoute hooks execution
    
    * improve hook validation
    
    * add test for hook registration by onRoute hook
    
    * use custom fastify error
    
    * reuse the existing hook error

--- test/hooks.test.js
+++ test/hooks.test.js
@@ -3301,3 +3301,41 @@ test('onTimeout should be triggered and socket _meta is set', t => {
     })
   })
 })
+
+test('registering invalid hooks should throw an error', async t => {
+  t.plan(3)
+
+  const fastify = Fastify()
+
+  t.throws(() => {
+    fastify.route({
+      method: 'GET',
+      path: '/invalidHook',
+      onRequest: [undefined],
+      async handler () {
+        return 'hello world'
+      }
+    })
+  }, new Error('onRequest hook should be a function, instead got undefined'))
+
+  t.throws(() => {
+    fastify.route({
+      method: 'GET',
+      path: '/invalidHook',
+      onRequest: undefined,
+      async handler () {
+        return 'hello world'
+      }
+    })
+  }, new Error('onRequest hook should be a function, instead got undefined'))
+
+  t.throws(() => {
+    fastify.addHook('onRoute', (routeOptions) => {
+      routeOptions.onSend = [undefined]
+    })
+
+    fastify.get('/', function (request, reply) {
+      reply.send('hello world')
+    })
+  }, new Error('onSend hook should be a function, instead got undefined'))
+})

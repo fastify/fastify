3a835d60ad028ff44a585105f927be6a1f4d7e01
Matteo Collina <hello@matteocollina.com>
2023-02-26 01:34:31 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { test, before } = require('tap')
+const { test, teardown, before } = require('tap')
 const helper = require('./helper')
 const http = require('http')
 const stream = require('stream')
@@ -13,18 +13,32 @@ const fs = require('fs')
 const sget = require('simple-get').concat
 const dns = require('dns')
 
+const files = []
 let count = 0
 let localhost
 let localhostForURL
 
 function file () {
   const file = path.join(os.tmpdir(), `sonic-boom-${process.pid}-${process.hrtime().toString()}-${count++}`)
+  files.push(file)
   return file
 }
 
 before(async function () {
   [localhost, localhostForURL] = await helper.getLoopbackHost()
 })
+
+if (process.env.CI) {
+  teardown(() => {
+    files.forEach((file) => {
+      try {
+        fs.unlinkSync(file)
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  })
+}
 
 test('defaults to info level', t => {
   let fastify = null

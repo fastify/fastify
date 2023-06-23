d8cd0a17cebaa5f67b1236b46b028e06a7decca1
Md Adil <md-adil@live.com>
2023-06-23 15:37:47 +0530

    pluginName will be exposed in FastifyInstance

--- types/instance.d.ts
+++ types/instance.d.ts
@@ -96,6 +96,7 @@ export interface FastifyInstance<
   TypeProvider extends FastifyTypeProvider = FastifyTypeProviderDefault,
 > {
   server: RawServer;
+  readonly pluginName?: string;
   prefix: string;
   version: string;
   log: Logger;

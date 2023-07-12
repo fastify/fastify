2f95e58529fa528df505d73f0074954dc020f6d5
Brenden Inhelder <96445076+BrendenInhelder@users.noreply.github.com>
2023-07-12 00:09:33 +0000

    Server.js changes for undefined listenOptions AND new basic onListen test file

--- test/hooks.on-listen.test.js
+++ test/hooks.on-listen.test.js
@@ -2,55 +2,43 @@
 
 const t = require('tap')
 const Fastify = require('../fastify')
-const immediate = require('util').promisify(setImmediate)
-//  commented out synchronous test, working on only async for now, the test doesn't work yet
 
-// t.test('onListen should be called in order', t => {
-//   console.log('start of test') //print
-//   t.plan(4)
-//   const fastify = Fastify()
+t.test('***onListen should be called in order***', t => {
+  t.plan(2)
 
-//   let order = 0
-//   console.log('start of first addHook') //  print
-//   fastify.addHook('onListen', function () {
-//     t.equal(order++, 0, 'called in root')
-//   })
-
-//   console.log('start of 2nd addHook') //    print
-//   fastify.addHook('onListen', function () {
-//     t.equal(order++, 1, 'called in root')
-//   })
-
-//   console.log('start of listen()') //print
-//   fastify.listen({ host: '::', port: 0 }, err => {
-//     console.log('start of error()') //  print
-//     t.error(err)
-//     console.log('start of teardown() and close()') //   print
-//     t.teardown(() => { fastify.close() })
-//     console.log('end of listen() and test') //   print
-//   })
-// })
-
-t.test('async onListen should be called in order', async t => {
-  console.log('log- start of async test')
-  t.plan(1) //  changed t.plan(1) to pass temporarily, but the two t.equal tests are not being asserted right now (line 43 and 49)
   const fastify = Fastify()
+  t.teardown(fastify.close.bind(fastify))
 
   let order = 0
 
+  fastify.addHook('onListen', function () {
+    t.equal(order++, 0, '1st called in root')
+  })
+
+  fastify.addHook('onListen', function () {
+    t.equal(order++, 1, '2nd called in root')
+  })
+  fastify.listen({
+    host: 'localhost',
+    port: 0
+  })
+})
+t.test('***async onListen should be called in order***', async t => {
+  t.plan(2)
+  const fastify = Fastify()
+  t.teardown(fastify.close.bind(fastify))
+  let order = 0
+
   fastify.addHook('onListen', async function () {
-    await immediate()
-    t.equal(order++, 0, 'called in root')
+    t.equal(order++, 0, '1st async called in root')
   })
 
-  fastify.addHook('onListen', async function () { //    seems to be addHook not working, since nothing in here is being printed
-    console.log('log-start of 2nd assertion')
-    await immediate()
-    t.equal(order++, 1, 'called in root') //  note how t.equal is called for the second time, but t.count==1, which only includes t.pass from line54
-    console.log('log-2nd assertion') // test
+  fastify.addHook('onListen', async function () {
+    t.equal(order++, 1, '2nd async called in root')
   })
 
-  await fastify.listen({ host: '::', port: 0 })
-  t.pass('onListen')
-  fastify.close() //    closes the port since it doesnt close without passing the tests as mentioned on line 46
+  await fastify.listen({
+    host: 'localhost',
+    port: 0
+  })
 })

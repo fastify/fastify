364f1d9bb3082210d1cc6a4a2a1e9dc173885698
uzlopak <aras.abbasi@googlemail.com>
2023-08-14 02:11:54 +0200

    fix typing test

--- test/types/hooks.test-d.ts
+++ test/types/hooks.test-d.ts
@@ -127,7 +127,7 @@ server.addHook('onRoute', function (opts) {
   expectType<RouteOptions & { routePath: string; path: string; prefix: string }>(opts)
 })
 
-server.addHook('onRegister', (opts, done) => {
+server.addHook('onRegister', function (opts, done) {
   expectType<FastifyInstance>(this)
   expectType<RegisterOptions & FastifyPluginOptions>(opts)
   expectAssignable<(err?: FastifyError) => void>(done)
@@ -214,7 +214,7 @@ server.addHook('onRequestAbort', async function (request) {
   expectType<FastifyRequest>(request)
 })
 
-server.addHook('onRegister', async (opts) => {
+server.addHook('onRegister', async function (opts) {
   expectType<FastifyInstance>(this)
   expectType<RegisterOptions & FastifyPluginOptions>(opts)
 })

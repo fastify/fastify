41605f496c24422fe25610668d031564c0331b0d
derammo <817160+derammo@users.noreply.github.com>
2023-12-05 09:17:35 -0500

    addSchema no longer breaks setValidatorCompiler
    custom validator compilers installed directly via setValidatorCompiler
    were removed on server start if any schema was previously added
    via addSchema
    
    new approach is to register any compiler from setValidatorCompiler
    or setSerializerCompiler as a custom builder, so everything
    is handled consistently
    
    implementation note: schema controllers by default alias their
    parent's compilers factory collection so you have to clone
    it to make local changes
    
    tests for 5178
    tests for custom validator replacement
    test for setSerializerCompiler with addSchema

--- test/schema-serialization.test.js
+++ test/schema-serialization.test.js
@@ -593,6 +593,47 @@ test('Custom setSerializerCompiler returns bad serialized output', t => {
   })
 })
 
+test('Custom setSerializerCompiler with addSchema', t => {
+  t.plan(6)
+  const fastify = Fastify({ exposeHeadRoutes: false })
+
+  const outSchema = {
+    $id: 'test',
+    type: 'object',
+    whatever: 'need to be parsed by the custom serializer'
+  }
+
+  fastify.setSerializerCompiler(({ schema, method, url, httpStatus }) => {
+    t.equal(method, 'GET')
+    t.equal(url, '/foo/:id')
+    t.equal(httpStatus, '200')
+    t.same(schema, outSchema)
+    return _data => JSON.stringify({ id: 2 })
+  })
+
+  // provoke re-creation of serialization compiler in setupSerializer
+  fastify.addSchema({ $id: 'dummy', type: 'object' })
+
+  fastify.get('/foo/:id', {
+    handler (_req, reply) {
+      reply.send({ id: 1 })
+    },
+    schema: {
+      response: {
+        200: outSchema
+      }
+    }
+  })
+
+  fastify.inject({
+    method: 'GET',
+    url: '/foo/123'
+  }, (err, res) => {
+    t.error(err)
+    t.equal(res.payload, JSON.stringify({ id: 2 }))
+  })
+})
+
 test('Custom serializer per route', async t => {
   const fastify = Fastify()
 

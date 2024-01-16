e5158e252d88d208ea739481d3ced228b42182ba
Adam Jones <domdomegg+git@gmail.com>
2024-01-16 02:03:50 +0000

    Better comment tests
    Because of how error handlers wrap things, following the control flow in the tets can be tricky. In this test file I've added numbered comments indicate the order statements are expected to execute. I've also standardised the names of things being thrown so it's easier to follow them.

--- test/encapsulated-error-handler.test.js
+++ test/encapsulated-error-handler.test.js
@@ -4,26 +4,39 @@ const { test } = require('tap')
 const Fastify = require('..')
 const split = require('split2')
 
+// Because of how error handlers wrap things, following the control flow can be tricky
+// In this test file numbered comments indicate the order statements are expected to execute
+
 test('encapuslates an error handler', async t => {
   t.plan(4)
 
   const fastify = Fastify()
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(async function a (err) {
-      t.equal(err.message, 'kaboom')
-      throw new Error('caught')
+      // 3. the inner error handler catches the error, and throws a new error
+      t.equal(err.message, 'from_endpoint')
+      throw new Error('from_inner')
     })
-    fastify.get('/encapsulated', async () => { throw new Error('kaboom') })
+    fastify.get('/encapsulated', async () => {
+      // 2. the endpoint throws an error
+      throw new Error('from_endpoint')
+    })
   })
 
   fastify.setErrorHandler(async function b (err) {
-    t.equal(err.message, 'caught')
+    // 4. the outer error handler catches the error thrown by the inner error handler
+    t.equal(err.message, 'from_inner')
+    // NB: no cause set for async error handlers because told not to add it to wrapThenable
+    // https://github.com/fastify/fastify/pull/5222#discussion_r1433848321
     t.notOk(err.cause)
-    throw new Error('wrapped')
+    // 5. the outer error handler throws a new error
+    throw new Error('from_outer')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
-  t.equal(res.json().message, 'wrapped')
+  // 6. the default error handler returns the error from the outer error handler
+  t.equal(res.json().message, 'from_outer')
 })
 
 // See discussion in https://github.com/fastify/fastify/pull/5222#discussion_r1432573655
@@ -33,20 +46,29 @@ test('bubbles up cause if error handler is synchronous', async t => {
   const fastify = Fastify()
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(function a (err) {
-      t.equal(err.message, 'kaboom')
-      throw new Error('caught')
+      // 3. the inner error handler catches the error, and throws a new error
+      t.equal(err.message, 'from_endpoint')
+      throw new Error('from_inner')
     })
-    fastify.get('/encapsulated', async () => { throw new Error('kaboom') })
+    fastify.get('/encapsulated', async () => {
+      // 2. the endpoint throws an error
+      throw new Error('from_endpoint')
+    })
   })
 
   fastify.setErrorHandler(async function b (err) {
-    t.equal(err.message, 'caught')
-    t.equal(err.cause?.message, 'kaboom')
-    throw new Error('wrapped')
+    // 4. the outer error handler catches the error thrown by the inner error handler
+    t.equal(err.message, 'from_inner')
+    // cause set to the error thrown by the inner function
+    t.equal(err.cause?.message, 'from_endpoint')
+    // 5. the outer error handler throws a new error
+    throw new Error('from_outer')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
-  t.equal(res.json().message, 'wrapped')
+  // 6. the default error handler returns the error from the outer error handler
+  t.equal(res.json().message, 'from_outer')
 })
 
 test('onError hook nested', async t => {
@@ -55,23 +77,32 @@ test('onError hook nested', async t => {
   const fastify = Fastify()
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(async function a (err) {
-      t.equal(err.message, 'kaboom')
-      throw new Error('caught')
+      // 4. the inner error handler catches the error, and throws a new error
+      t.equal(err.message, 'from_endpoint')
+      throw new Error('from_inner')
     })
-    fastify.get('/encapsulated', async () => { throw new Error('kaboom') })
+    fastify.get('/encapsulated', async () => {
+      // 2. the endpoint throws an error
+      throw new Error('from_endpoint')
+    })
   })
 
   fastify.setErrorHandler(async function b (err) {
-    t.equal(err.message, 'caught')
-    throw new Error('wrapped')
+    // 5. the outer error handler catches the error thrown by the inner error handler
+    t.equal(err.message, 'from_inner')
+    // 6. the outer error handler throws a new error
+    throw new Error('from_outer')
   })
 
   fastify.addHook('onError', async function (request, reply, err) {
-    t.equal(err.message, 'kaboom')
+    // 3. the hook receives the error
+    t.equal(err.message, 'from_endpoint')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
-  t.equal(res.json().message, 'wrapped')
+  // 7. the default error handler returns the error from the outer error handler
+  t.equal(res.json().message, 'from_outer')
 })
 
 // See https://github.com/fastify/fastify/issues/5220
@@ -81,19 +112,27 @@ test('encapuslates an error handler, for errors thrown in hooks', async t => {
   const fastify = Fastify()
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(function a (err) {
+      // 3. the inner error handler catches the error, and throws a new error
       t.equal(err.message, 'from_hook')
       throw new Error('from_inner')
     })
-    fastify.addHook('onRequest', async () => { throw new Error('from_hook') })
+    fastify.addHook('onRequest', async () => {
+      // 2. the hook throws an error
+      throw new Error('from_hook')
+    })
     fastify.get('/encapsulated', async () => {})
   })
 
   fastify.setErrorHandler(function b (err) {
+    // 4. the outer error handler catches the error thrown by the inner error handler
     t.equal(err.message, 'from_inner')
+    // 5. the outer error handler throws a new error
     throw new Error('from_outer')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
+  // 6. the default error handler returns the error from the outer error handler
   t.equal(res.json().message, 'from_outer')
 })
 
@@ -102,17 +141,34 @@ test('encapuslates many synchronous error handlers that rethrow errors', async t
   const DEPTH = 100
   t.plan(DEPTH + 2)
 
+  /**
+   * This creates a very nested set of error handlers, that looks like:
+   * plugin
+   * - error handler
+   * - plugin
+   *   - error handler
+   *   - plugin
+   *     ... {to DEPTH levels}
+   *       - plugin
+   *           - error handler
+   *           - GET /encapsulated
+   */
   const createNestedRoutes = (fastify, depth) => {
     if (depth < 0) {
       throw new Error('Expected depth >= 0')
     } else if (depth === 0) {
       fastify.setErrorHandler(function a (err) {
+        // 3. innermost error handler catches the error, and throws a new error
         t.equal(err.message, 'from_route')
         throw new Error(`from_handler_${depth}`)
       })
-      fastify.get('/encapsulated', async () => { throw new Error('from_route') })
+      fastify.get('/encapsulated', async () => {
+        // 2. the endpoint throws an error
+        throw new Error('from_route')
+      })
     } else {
       fastify.setErrorHandler(function d (err) {
+        // 4 to {DEPTH+4}. error handlers each catch errors, and then throws a new error
         t.equal(err.message, `from_handler_${depth - 1}`)
         throw new Error(`from_handler_${depth}`)
       })
@@ -126,7 +182,9 @@ test('encapuslates many synchronous error handlers that rethrow errors', async t
   const fastify = Fastify()
   createNestedRoutes(fastify, DEPTH)
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
+  // {DEPTH+5}. the default error handler returns the error from the outermost error handler
   t.equal(res.json().message, `from_handler_${DEPTH}`)
 })
 
@@ -137,17 +195,34 @@ test('encapuslates many asynchronous error handlers that rethrow errors', async 
   const DEPTH = 100
   t.plan(DEPTH + 2)
 
+  /**
+   * This creates a very nested set of error handlers, that looks like:
+   * plugin
+   * - error handler
+   * - plugin
+   *   - error handler
+   *   - plugin
+   *     ... {to DEPTH levels}
+   *       - plugin
+   *           - error handler
+   *           - GET /encapsulated
+   */
   const createNestedRoutes = (fastify, depth) => {
     if (depth < 0) {
       throw new Error('Expected depth >= 0')
     } else if (depth === 0) {
       fastify.setErrorHandler(async function a (err) {
+        // 3. innermost error handler catches the error, and throws a new error
         t.equal(err.message, 'from_route')
         throw new Error(`from_handler_${depth}`)
       })
-      fastify.get('/encapsulated', async () => { throw new Error('from_route') })
+      fastify.get('/encapsulated', async () => {
+        // 2. the endpoint throws an error
+        throw new Error('from_route')
+      })
     } else {
       fastify.setErrorHandler(async function m (err) {
+        // 4 to {DEPTH+4}. error handlers each catch errors, and then throws a new error
         t.equal(err.message, `from_handler_${depth - 1}`)
         throw new Error(`from_handler_${depth}`)
       })
@@ -161,12 +236,14 @@ test('encapuslates many asynchronous error handlers that rethrow errors', async 
   const fastify = Fastify()
   createNestedRoutes(fastify, DEPTH)
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
+  // {DEPTH+5}. the default error handler returns the error from the outermost error handler
   t.equal(res.json().message, `from_handler_${DEPTH}`)
 })
 
 test('handles error handler throwing a frozen error', async t => {
-  t.plan(4)
+  t.plan(5)
 
   const logStream = split(JSON.parse)
   const messages = []
@@ -182,21 +259,33 @@ test('handles error handler throwing a frozen error', async t => {
   })
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(function a (err) {
-      t.equal(err.message, 'kaboom')
-      const e = new Error('caught')
+      // 3. the inner error handler catches the error, and throws a new frozen error
+      t.equal(err.message, 'from_endpoint')
+      const e = new Error('from_inner')
       Object.freeze(e)
       throw e
     })
-    fastify.get('/encapsulated', async () => { throw new Error('kaboom') })
+    fastify.get('/encapsulated', async () => {
+      // 2. the endpoint throws an error
+      throw new Error('from_endpoint')
+    })
   })
 
-  fastify.setErrorHandler(async function b (err) {
-    t.equal(err.message, 'caught')
-    throw new Error('wrapped')
+  fastify.setErrorHandler(function b (err) {
+    // 5. the outer error handler catches the error thrown by the inner error handler
+    t.equal(err.message, 'from_inner')
+    // no cause set for async error handlers because this error is frozen
+    t.notOk(err.cause)
+    // 6. the outer error handler throws a new error
+    throw new Error('from_outer')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
-  t.equal(res.json().message, 'wrapped')
+  // 7. the default error handler returns the error from the outer error handler
+  t.equal(res.json().message, 'from_outer')
+  // 4. the child cause cannot be assigned when passing the error from the inner to
+  // outer error handler, because it is frozen, so we log a warning.
   t.ok(messages.find(message => message.message?.includes('Failed to assign child error')))
 })
 
@@ -217,21 +306,32 @@ test('handles error handler throwing an error already with a cause', async t => 
   })
   fastify.register(async function (fastify) {
     fastify.setErrorHandler(function a (err) {
-      t.equal(err.message, 'kaboom')
-      const e = new Error('caught')
-      e.cause = new Error('existing cause')
+      // 3. the inner error handler catches the error, and throws a new error with a custom cause
+      t.equal(err.message, 'from_endpoint')
+      const e = new Error('from_inner')
+      e.cause = new Error('from_inner_custom_cause')
       throw e
     })
-    fastify.get('/encapsulated', async () => { throw new Error('kaboom') })
+    fastify.get('/encapsulated', async () => {
+      // 2. the endpoint throws an error
+      throw new Error('from_endpoint')
+    })
   })
 
-  fastify.setErrorHandler(async function b (err) {
-    t.equal(err.message, 'caught')
-    t.equal(err.cause.message, 'existing cause')
-    throw new Error('wrapped')
+  fastify.setErrorHandler(function b (err) {
+    // 5. the outer error handler catches the error thrown by the inner error handler
+    t.equal(err.message, 'from_inner')
+    // cause is not modified because it is already set
+    t.equal(err.cause.message, 'from_inner_custom_cause')
+    // 6. the outer error handler throws a new error
+    throw new Error('from_outer')
   })
 
+  // 1. the endpoint is called
   const res = await fastify.inject('/encapsulated')
-  t.equal(res.json().message, 'wrapped')
+  // 7. the default error handler returns the error from the outer error handler
+  t.equal(res.json().message, 'from_outer')
+  // 4. the child cause is not assigned when passing the error from the inner to
+  // outer error handler, because it would overwrite an existing cause, so we log a warning.
   t.ok(messages.find(message => message.message?.includes('Failed to assign child error')))
 })

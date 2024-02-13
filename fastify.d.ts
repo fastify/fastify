094871616b7766860102043627921186d1a2601e
Bram del Canho <bramdelcanho@gmail.com>
2024-02-13 10:32:10 +0100

    Added FastifySeparatedTypeProvider export to fastify.d.ts
    Signed-off-by: Bram del Canho <bramdelcanho@gmail.com>

--- fastify.d.ts
+++ fastify.d.ts
@@ -22,7 +22,7 @@ import { FastifyRequest, RequestGenericInterface } from './types/request'
 import { RouteHandler, RouteHandlerMethod, RouteOptions, RouteShorthandMethod, RouteShorthandOptions, RouteShorthandOptionsWithHandler, RouteGenericInterface } from './types/route'
 import { FastifySchema, FastifySchemaCompiler, FastifySchemaValidationError, SchemaErrorDataVar, SchemaErrorFormatter } from './types/schema'
 import { FastifyServerFactory, FastifyServerFactoryHandler } from './types/serverFactory'
-import { FastifyTypeProvider, FastifyTypeProviderDefault } from './types/type-provider'
+import { FastifyTypeProvider, FastifySeparatedTypeProvider, FastifyTypeProviderDefault } from './types/type-provider'
 import { HTTPMethods, RawServerBase, RawRequestDefaultExpression, RawReplyDefaultExpression, RawServerDefault, ContextConfigDefault, RequestBodyDefault, RequestQuerystringDefault, RequestParamsDefault, RequestHeadersDefault } from './types/utils'
 
 declare module '@fastify/error' {

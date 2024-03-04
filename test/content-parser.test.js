d3d75752e9f324d47ff95a471034f9fbaecf1184
Gürgün Dayıoğlu <hey@gurgun.day>
2024-03-04 10:59:21 +0100

    align hasParser

--- test/content-parser.test.js
+++ test/content-parser.test.js
@@ -12,13 +12,14 @@ const third = function (req, payload, done) {}
 
 test('hasContentTypeParser', t => {
   test('should know about internal parsers', t => {
-    t.plan(4)
+    t.plan(5)
 
     const fastify = Fastify()
     fastify.ready(err => {
       t.error(err)
       t.ok(fastify.hasContentTypeParser('application/json'))
       t.ok(fastify.hasContentTypeParser('text/plain'))
+      t.ok(fastify.hasContentTypeParser('  text/plain  '))
       t.notOk(fastify.hasContentTypeParser('application/jsoff'))
     })
   })

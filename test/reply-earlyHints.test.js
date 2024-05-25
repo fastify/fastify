064aea5a6a3f8aa54c2350b23f76098d49fb2972
Gürgün Dayıoğlu <hey@gurgun.day>
2024-05-25 18:45:59 +0200

    feat: writeEarlyHints

--- test/reply-earlyHints.test.js
+++ test/reply-earlyHints.test.js
@@ -1,12 +1,58 @@
 'use strict'
 
 const Fastify = require('..')
-const tap = require('tap')
+const { test } = require('tap')
+const http = require('http')
 const http2 = require('http2')
 
 const testResBody = 'Hello, world!'
 
-tap.test('Happy flow - string argument', (t) => {
+test('sends early hints', (t) => {
+  t.plan(6)
+
+  const fastify = Fastify({
+    logger: false
+  })
+
+  fastify.get('/', async (request, reply) => {
+    reply.writeEarlyHints({
+      link: '</styles.css>; rel=preload; as=style'
+    }, () => {
+      t.pass('callback called')
+    })
+
+    return testResBody
+  })
+
+  fastify.listen({ port: 0 }, (err, address) => {
+    t.error(err)
+
+    const req = http.get(address)
+
+    req.on('information', (res) => {
+      t.equal(res.statusCode, 103)
+      t.equal(res.headers.link, '</styles.css>; rel=preload; as=style')
+    })
+
+    req.on('response', (res) => {
+      t.equal(res.statusCode, 200)
+
+      let data = ''
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+
+      res.on('end', () => {
+        t.equal(data, testResBody)
+        fastify.close(t.end)
+      })
+    })
+  })
+})
+
+test('sends early hints (http2)', (t) => {
+  t.plan(6)
+
   const fastify = Fastify({
     http2: true,
     logger: false

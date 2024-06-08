0cab68b5c51919a2f52549a770f831ac9de9f09e
Matteo Collina <hello@matteocollina.com>
2024-06-08 17:44:22 +0200

    Update eslint.config.js
    Co-authored-by: Pelle Wessman <pelle@kodfabrik.se>
    Signed-off-by: Matteo Collina <matteo.collina@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,18 @@ module.exports = [
   }),
   {
     ignores: [
+      'lib/configValidator.js',
+      'lib/error-serializer.js',
       'test/same-shape.test.js',
     ],
+    rules: {
+      '@stylistic/comma-dangle': ['error', {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      }],
+    }
   },
 ]

58563d46368548c7385f425b0f022c89031819fd
Matteo Collina <hello@matteocollina.com>
2024-06-09 15:37:40 +0300

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/build/error-serializer.test.js
+++ test/build/error-serializer.test.js
@@ -4,6 +4,7 @@ const t = require('tap')
 const test = t.test
 const fs = require('node:fs')
 const path = require('node:path')
+const { loadESLint } = require('eslint')
 
 const { code } = require('../../build/build-error-serializer')
 
@@ -15,8 +16,9 @@ test('check generated code syntax', async (t) => {
   t.plan(1)
 
   // standard is a esm, we import it like this
-  const { default: standard } = await import('standard')
-  const result = await standard.lintText(code)
+  const Eslint = await loadESLint({ useFlatConfig: true })
+  const eslint = new Eslint()
+  const result = await eslint.lintText(code)
 
   // if there are any invalid syntax
   // fatal count will be greater than 0

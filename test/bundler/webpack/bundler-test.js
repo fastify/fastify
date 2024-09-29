339c46e76d3d6246233e9d673eccf92c8294e4bd
Ran Toledo <toledoran@gmail.com>
2024-09-29 17:41:56 +0300

    added-diagnostic-channel-and-bundler-tests

--- test/bundler/webpack/bundler-test.js
+++ test/bundler/webpack/bundler-test.js
@@ -1,23 +1,22 @@
 'use strict'
 
-const t = require('tap')
-const test = t.test
+const { test } = require('node:test')
 const fastifySuccess = require('./dist/success')
 const fastifyFailPlugin = require('./dist/failPlugin')
 
 test('Bundled package should work', (t) => {
   t.plan(4)
   fastifySuccess.ready((err) => {
-    t.error(err)
+    t.assert.ifError(err)
     fastifySuccess.inject(
       {
         method: 'GET',
         url: '/'
       },
       (error, res) => {
-        t.error(error)
-        t.equal(res.statusCode, 200)
-        t.same(res.json(), { hello: 'world' })
+        t.assert.ifError(error)
+        t.assert.strictEqual(res.statusCode, 200)
+        t.assert.deepStrictEqual(res.json(), { hello: 'world' })
       }
     )
   })
@@ -26,6 +25,6 @@ test('Bundled package should work', (t) => {
 test('Bundled package should not work with bad plugin version', (t) => {
   t.plan(1)
   fastifyFailPlugin.ready((err) => {
-    t.match(err.message, /expected '9.x' fastify version/i)
+    t.assert.match(err.message, /expected '9.x' fastify version/i)
   })
 })

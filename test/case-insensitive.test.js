f02ab7a79bc53a2e9716b640d7a791b646e67551
Roberto Stagi <34033081+rstagi@users.noreply.github.com>
2024-10-26 21:36:15 +0200

    test: change deepEqual into deepStrictEqual
    Co-authored-by: Gürgün Dayıoğlu <hey@gurgun.day>
    Signed-off-by: Roberto Stagi <34033081+rstagi@users.noreply.github.com>

--- test/case-insensitive.test.js
+++ test/case-insensitive.test.js
@@ -114,7 +114,7 @@ test('case insensitive (wildcard)', (t, done) => {
     }, (err, response, body) => {
       t.assert.ifError(err)
       t.assert.strictEqual(response.statusCode, 200)
-      t.assert.deepEqual(JSON.parse(body), {
+      t.assert.deepStrictEqual(JSON.parse(body), {
         hello: 'world'
       })
       done()

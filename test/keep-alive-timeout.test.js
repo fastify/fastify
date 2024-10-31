8e0008511c677045d95618c9928a8906ed701215
Antonio Tripodi <Tony133@users.noreply.github.com>
2024-10-31 13:14:42 +0100

    test: migrated keep-alive-timeout.test.js from tap to node:test (#5796)
    * test: migrated keepAliveTimeout.test.js from tap to node:test
    
    * test: renamed file to kabab-case

--- test/keep-alive-timeout.test.js
+++ test/keep-alive-timeout.test.js
@@ -2,34 +2,33 @@
 
 const Fastify = require('..')
 const http = require('node:http')
-const t = require('tap')
-const test = t.test
+const { test } = require('node:test')
 
 test('keepAliveTimeout', t => {
   t.plan(6)
 
   try {
     Fastify({ keepAliveTimeout: 1.3 })
-    t.fail('option must be an integer')
+    t.assert.fail('option must be an integer')
   } catch (err) {
-    t.ok(err)
+    t.assert.ok(err)
   }
 
   try {
     Fastify({ keepAliveTimeout: [] })
-    t.fail('option must be an integer')
+    t.assert.fail('option must be an integer')
   } catch (err) {
-    t.ok(err)
+    t.assert.ok(err)
   }
 
   const httpServer = Fastify({ keepAliveTimeout: 1 }).server
-  t.equal(httpServer.keepAliveTimeout, 1)
+  t.assert.strictEqual(httpServer.keepAliveTimeout, 1)
 
   const httpsServer = Fastify({ keepAliveTimeout: 2, https: {} }).server
-  t.equal(httpsServer.keepAliveTimeout, 2)
+  t.assert.strictEqual(httpsServer.keepAliveTimeout, 2)
 
   const http2Server = Fastify({ keepAliveTimeout: 3, http2: true }).server
-  t.not(http2Server.keepAliveTimeout, 3)
+  t.assert.notStrictEqual(http2Server.keepAliveTimeout, 3)
 
   const serverFactory = (handler, _) => {
     const server = http.createServer((req, res) => {
@@ -39,5 +38,5 @@ test('keepAliveTimeout', t => {
     return server
   }
   const customServer = Fastify({ keepAliveTimeout: 4, serverFactory }).server
-  t.equal(customServer.keepAliveTimeout, 5)
+  t.assert.strictEqual(customServer.keepAliveTimeout, 5)
 })

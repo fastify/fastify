c3256f8cf54a38612b85f9a17c90219c171ce1e2
Nemanja Tosic <nemanja@livingspec.com>
2024-11-13 16:52:39 +0100

    Use alias for FastifyRegister
    Using the instance method because it is easier to infer types.

--- types/register.d.ts
+++ types/register.d.ts
@@ -49,33 +49,9 @@ export type EnhanceArray<U extends AnyFastifyInstance, T extends FastifyDependen
       : U
     : U
 
-export type FastifyRegister = FastifyInstance['register']
-
 /**
  * FastifyRegister
  *
  * Function for adding a plugin to fastify. The options are inferred from the passed in FastifyPlugin parameter.
  */
-// export interface FastifyRegister<T = void, RawServer extends RawServerBase = RawServerDefault, TypeProviderDefault extends FastifyTypeProvider = FastifyTypeProvider, LoggerDefault extends FastifyBaseLogger = FastifyBaseLogger> {
-//   <Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginCallback<FastifyPluginOptions, Server, TypeProvider, Logger>
-//   ): T;
-//   <Options extends FastifyPluginOptions, Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginCallback<Options, Server, TypeProvider, Logger>,
-//     opts: FastifyRegisterOptions<Options>
-//   ): T;
-//   <Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginAsync<FastifyPluginOptions, Server, TypeProvider, Logger>
-//   ): T;
-//   <Options extends FastifyPluginOptions, Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginAsync<Options, Server, TypeProvider, Logger>,
-//     opts: FastifyRegisterOptions<Options>
-//   ): T;
-//   <Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginCallback<FastifyPluginOptions, Server, TypeProvider, Logger> | FastifyPluginAsync<FastifyPluginOptions, Server, TypeProvider, Logger> | Promise<{ default: FastifyPluginCallback<FastifyPluginOptions, Server, TypeProvider, Logger> }> | Promise<{ default: FastifyPluginAsync<FastifyPluginOptions, Server, TypeProvider, Logger> }>,
-//   ): T;
-//   <Options extends FastifyPluginOptions, Server extends RawServerBase = RawServer, TypeProvider extends FastifyTypeProvider = TypeProviderDefault, Logger extends FastifyBaseLogger = LoggerDefault>(
-//     plugin: FastifyPluginCallback<Options, Server, TypeProvider, Logger> | FastifyPluginAsync<Options, Server, TypeProvider, Logger> | Promise<{ default: FastifyPluginCallback<Options, Server, TypeProvider, Logger> }> | Promise<{ default: FastifyPluginAsync<Options, Server, TypeProvider, Logger> }>,
-//     opts: FastifyRegisterOptions<Options>
-//   ): T;
-// }
+export type FastifyRegister = FastifyInstance['register']

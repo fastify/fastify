3ea5fa91a403407684d52209fe2d026b812563a6
Reidner <reidnersteam@hotmail.com>
2024-11-21 13:27:14 -0300

    test: migrated sync-routes.test.js from tap to node:test

--- test/sync-routes.test.js
+++ test/sync-routes.test.js
@@ -1,32 +1,32 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 const Fastify = require('..')
 
 test('sync route', async t => {
   const app = Fastify()
-  t.teardown(app.close.bind(app))
+  t.after(() => app.close.bind(app))
   app.get('/', () => 'hello world')
   const res = await app.inject('/')
-  t.equal(res.statusCode, 200)
-  t.equal(res.body, 'hello world')
+  t.assert.strictEqual(res.statusCode, 200)
+  t.assert.strictEqual(res.body, 'hello world')
 })
 
 test('sync route return null', async t => {
   const app = Fastify()
-  t.teardown(app.close.bind(app))
+  t.after(() => app.close.bind(app))
   app.get('/', () => null)
   const res = await app.inject('/')
-  t.equal(res.statusCode, 200)
-  t.equal(res.body, 'null')
+  t.assert.strictEqual(res.statusCode, 200)
+  t.assert.strictEqual(res.body, 'null')
 })
 
 test('sync route, error', async t => {
   const app = Fastify()
-  t.teardown(app.close.bind(app))
+  t.after(() => app.close.bind(app))
   app.get('/', () => {
     throw new Error('kaboom')
   })
   const res = await app.inject('/')
-  t.equal(res.statusCode, 500)
+  t.assert.strictEqual(res.statusCode, 500)
 })

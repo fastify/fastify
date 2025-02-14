bb1b8ffab709c77eb73598db35e9a6e57e597dd0
dan.castillo <dancastillo99@gmail.com>
2025-02-13 20:44:03 -0500

    feat: move router options to own key

--- test/use-semicolon-delimiter.test.js
+++ test/use-semicolon-delimiter.test.js
@@ -13,7 +13,7 @@ test('use semicolon delimiter default false', (t, done) => {
     reply.send(req.query)
   })
 
-  fastify.listen({ port: 0, }, err => {
+  fastify.listen({ port: 0 }, err => {
     t.assert.ifError(err)
     sget({
       method: 'GET',
@@ -88,6 +88,117 @@ test('use semicolon delimiter set to false return 404', (t, done) => {
 
   const fastify = Fastify({
     useSemicolonDelimiter: false
+  })
+
+  fastify.get('/1234', (req, reply) => {
+    reply.send(req.query)
+  })
+
+  fastify.listen({ port: 0 }, err => {
+    t.assert.ifError(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/1234;foo=bar'
+    }, (err, response, body) => {
+      t.assert.ifError(err)
+      t.assert.strictEqual(response.statusCode, 404)
+      fastify.close()
+      done()
+    })
+  })
+})
+
+test('use routerOptions semicolon delimiter default false', (t, done) => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.get('/1234;foo=bar', (req, reply) => {
+    reply.send(req.query)
+  })
+
+  fastify.listen({ port: 0 }, err => {
+    t.assert.ifError(err)
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/1234;foo=bar'
+    }, (err, response, body) => {
+      t.assert.ifError(err)
+      t.assert.strictEqual(response.statusCode, 200)
+      t.assert.deepStrictEqual(JSON.parse(body), {})
+      fastify.close()
+      done()
+    })
+  })
+})
+
+test('use routerOptions semicolon delimiter set to true', (t, done) => {
+  t.plan(4)
+  const fastify = Fastify({
+    routerOptions: {
+      useSemicolonDelimiter: true
+    }
+  })
+
+  fastify.get('/1234', (req, reply) => {
+    reply.send(req.query)
+  })
+
+  fastify.listen({ port: 0 }, err => {
+    t.assert.ifError(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/1234;foo=bar'
+    }, (err, response, body) => {
+      t.assert.ifError(err)
+      t.assert.strictEqual(response.statusCode, 200)
+      t.assert.deepStrictEqual(JSON.parse(body), {
+        foo: 'bar'
+      })
+      fastify.close()
+      done()
+    })
+  })
+})
+
+test('use routerOptions semicolon delimiter set to false', (t, done) => {
+  t.plan(4)
+
+  const fastify = Fastify({
+    routerOptions: {
+      useSemicolonDelimiter: false
+    }
+  })
+
+  fastify.get('/1234;foo=bar', (req, reply) => {
+    reply.send(req.query)
+  })
+
+  fastify.listen({ port: 0 }, err => {
+    t.assert.ifError(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/1234;foo=bar'
+    }, (err, response, body) => {
+      t.assert.ifError(err)
+      t.assert.strictEqual(response.statusCode, 200)
+      t.assert.deepStrictEqual(JSON.parse(body), {})
+      fastify.close()
+      done()
+    })
+  })
+})
+
+test('use routerOptions semicolon delimiter set to false return 404', (t, done) => {
+  t.plan(3)
+
+  const fastify = Fastify({
+    routerOptions: {
+      useSemicolonDelimiter: false
+    }
   })
 
   fastify.get('/1234', (req, reply) => {

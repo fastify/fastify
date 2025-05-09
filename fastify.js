91aafe9d5c46f256b50a6e208686ef2ec0ec317f
jean <jean.antoine.michelet@gmail.com>
2025-05-09 15:52:31 +0200

    docs: add JS doc

--- fastify.js
+++ fastify.js
@@ -199,7 +199,6 @@ function fastify (options) {
   // === INSTANCE OBJECT =================================================
   //
   // Construct the main `fastify` object: public API, symbol-backed
-  //
   const fastify = {
     // Fastify internals
     [kState]: {
@@ -436,20 +435,10 @@ function fastify (options) {
     fastify[kSchemaErrorFormatter] = options.schemaErrorFormatter.bind(fastify)
   }
 
-  // HTTP injection handling
-  // If the server is not ready yet, this
-  // utility will automatically force it.
   fastify.inject = setupInject(fastify, {
     httpHandler
   })
 
-  // Install and configure Avvio
-  // Avvio will update the following Fastify methods:
-  // - register
-  // - after
-  // - ready
-  // - onClose
-  // - close
   const avvio = setupAvvio(fastify, {
     options,
     defaultInitOptions,

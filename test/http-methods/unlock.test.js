99e51bb55af1ef46770a8ac5f07d20497d970363
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-06-07 18:40:16 +0200

    chore: removed simple-get from unlock test

--- test/http-methods/unlock.test.js
+++ test/http-methods/unlock.test.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const { test } = require('node:test')
-const sget = require('simple-get').concat
 const fastify = require('../../fastify')()
 fastify.addHttpMethod('UNLOCK')
 
@@ -22,21 +21,18 @@ test('can be created - unlock', t => {
 })
 
 test('unlock test', async t => {
-  await fastify.listen({ port: 0 })
+  const fastifyServer = await fastify.listen({ port: 0 })
 
   t.after(() => { fastify.close() })
-  await t.test('request - unlock', (t, done) => {
+  await t.test('request - unlock', async t => {
     t.plan(2)
-    sget({
-      url: `http://localhost:${fastify.server.address().port}/test/a.txt`,
+    const result = await fetch(`${fastifyServer}/test/a.txt`, {
       method: 'UNLOCK',
       headers: {
         'Lock-Token': 'urn:uuid:a515cfa4-5da4-22e1-f5b5-00a0451e6bf7'
       }
-    }, (err, response, body) => {
-      t.assert.ifError(err)
-      t.assert.strictEqual(response.statusCode, 204)
-      done()
     })
+    t.assert.ok(result.ok)
+    t.assert.strictEqual(result.status, 204)
   })
 })

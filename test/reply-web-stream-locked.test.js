6c565efae7f81bb53c694df337ebffd060646be9
Aras Abbasi <aras.abbasi@googlemail.com>
2025-09-26 00:33:24 +0200

    fix test

--- test/reply-web-stream-locked.test.js
+++ test/reply-web-stream-locked.test.js
@@ -1,27 +1,37 @@
 'use strict'
 
-const t = require('tap')
+const { ReadableStream } = require('node:stream/web')
+const { test, after } = require('node:test')
 const Fastify = require('..')
-const { ReadableStream } = require('stream/web')
 
-t.test('reply.send(web ReadableStream) throws if locked', async t => {
+test('reply.send(web ReadableStream) throws if locked', async t => {
+  t.plan(3)
+
   const app = Fastify()
+  after(() => app.close())
 
-  app.get('/', async (req, reply) => {
+  app.get('/', (req, reply) => {
     const rs = new ReadableStream({
       start (controller) { controller.enqueue(new TextEncoder().encode('hi')); controller.close() }
     })
     // lock the stream
     const reader = rs.getReader()
-    t.ok(rs.locked, 'stream is locked')
+    t.assert.strictEqual(rs.locked, true, 'stream is locked')
 
     // sending a locked stream should trigger the Fastify error
     reply.send(rs)
-    await reader.releaseLock()
+    reader.releaseLock()
   })
 
   const res = await app.inject({ method: 'GET', url: '/' })
-  t.equal(res.statusCode, 500)
-  t.match(res.body, /locked/i) // message mentions "locked"
-  await app.close()
+  t.assert.strictEqual(res.statusCode, 500)
+  t.assert.deepStrictEqual(
+    JSON.parse(res.body),
+    {
+      statusCode: 500,
+      code: 'FST_ERR_REP_READABLE_STREAM_LOCKED',
+      error: 'Internal Server Error',
+      message: 'ReadableStream was locked. You should call releaseLock() method on reader before sending.'
+    }
+  )
 })

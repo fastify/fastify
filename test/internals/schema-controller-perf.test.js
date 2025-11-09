46c7adeba09e478c96fabd8b4e6d5c331c914004
Manuel Spigolon <manuel.spigolon@nearform.com>
2025-11-09 13:18:07 +0100

    test

--- test/internals/schema-controller-perf.test.js
+++ test/internals/schema-controller-perf.test.js
@@ -1,4 +1,4 @@
-const os = require('node:os')
+const { sep } = require('node:path')
 const { test } = require('node:test')
 const Fastify = require('../../fastify')
 
@@ -15,8 +15,8 @@ test('SchemaController are NOT loaded when the controllers are custom', async t 
   await app.ready()
 
   const loaded = Object.keys(require.cache)
-  const ajvModule = loaded.find((path) => path.includes(`@fastify${os.separator}ajv-compiler`))
-  const stringifyModule = loaded.find((path) => path.includes(`@fastify${os.separator}fast-json-stringify-compiler`))
+  const ajvModule = loaded.find((path) => path.includes(`@fastify${sep}ajv-compiler`))
+  const stringifyModule = loaded.find((path) => path.includes(`@fastify${sep}fast-json-stringify-compiler`))
 
   t.assert.equal(ajvModule, undefined, 'Ajv compiler is loaded')
   t.assert.equal(stringifyModule, undefined, 'Stringify compiler is loaded')
@@ -27,8 +27,8 @@ test('SchemaController are loaded when the controllers are not custom', async t 
   await app.ready()
 
   const loaded = Object.keys(require.cache)
-  const ajvModule = loaded.find((path) => path.includes('@fastify/ajv-compiler'))
-  const stringifyModule = loaded.find((path) => path.includes('@fastify/fast-json-stringify-compiler'))
+  const ajvModule = loaded.find((path) => path.includes(`@fastify${sep}ajv-compiler`))
+  const stringifyModule = loaded.find((path) => path.includes(`@fastify${sep}fast-json-stringify-compiler`))
 
   t.after(() => {
     delete require.cache[ajvModule]

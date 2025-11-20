64638004ce5a4610f9a5122bb8aab1b7cd9cc5c0
kdrewes <kdrewes@kdrewess-MacBook-Pro.local>
2025-11-20 12:30:54 -0800

    Added demo applications

---
+++ examples/custom-logger-plugin.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const fastify = require('../fastify')({
+  logger: true,
+  disableRequestLogging: false // Enable logging so plugin can handle it
+})
+
+
+// Plugin function - this is what developers would create
+function customLogger (fastify, options, done) {
+  fastify.addHook('onRequest', (request, reply, done) => {
+    request.log.info({ 
+      req: request,
+      timestamp: new Date().toISOString(),
+      method: request.method,
+      url: request.url,
+      ip: request.ip
+    }, 'Custom Request Log')
+    done()
+  })
+  done()
+}
+
+// Register the plugin
+fastify.register(customLogger)
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+fastify.listen({ port: 3000 }, (err) => {
+  if (err) throw err
+})

8c3ae4649ae9d806403b3e9e5cace15e6dc57e6e
Ankan Misra <misra13arko@gmail.com>
2025-11-29 22:59:08 +0530

    fix: use cross-platform regex for schema prettification

--- build/build-validation.js
+++ build/build-validation.js
@@ -10,7 +10,7 @@ const factory = AjvStandaloneCompiler({
   storeFunction (routeOpts, schemaValidationCode) {
     // Prettify the schema definition to make diffs readable
     schemaValidationCode = schemaValidationCode.replace(
-      /const schema\d+ = \{.*?\};/gs,
+      /const schema\d+ = \{[\s\S]*?\};/g,
       (match) => {
         try {
           const prefix = match.match(/const schema\d+ = /)[0]

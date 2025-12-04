0f7fc7604b84ef6105712494cfc49cf02544b0e4
Neha-Sharmaaa <neha.k@adypu.edu.in>
2025-12-04 23:18:36 +0530

    fix

--- examples/plugin.js
+++ examples/plugin.js
@@ -1,12 +1,13 @@
 'use strict'
 
 module.exports = function (fastify, opts, done) {
-  fastify
-    .get('/', opts, function (req, reply) {
-      reply.send({ hello: 'world' })
-    })
-    .post('/', opts, function (req, reply) {
-      reply.send({ hello: 'world' })
-    })
+  fastify.get('/', opts, function (req, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.post('/', opts, function (req, reply) {
+    reply.send({ hello: 'world' })
+  })
+
   done()
 }

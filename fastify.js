5ef5a4de5f9084b616ba77769f0e3615d23875d0
Manuel Spigolon <manuel.spigolon@nearform.com>
2026-06-28 09:22:22 +0200

    ops

--- fastify.js
+++ fastify.js
@@ -645,7 +645,7 @@ function fastify (serverOptions) {
       const request = new Request(id, null, req, null, childLogger, routeEventContext)
       const reply = new Reply(res, request, childLogger)
 
-      onBadUrlContext.server[kLogController].incomingRequest(request)
+      routeEventContext.server[kLogController].incomingRequest(request)
 
       return options.frameworkErrors(new FST_ERR_BAD_URL(path), request, reply)
     }
@@ -670,10 +670,7 @@ function fastify (serverOptions) {
       const request = new Request(id, null, req, null, childLogger, routeEventContext)
       const reply = new Reply(res, request, childLogger)
 
-      const resolvedDisableRequestLogging = typeof disableRequestLogging === 'function' ? disableRequestLogging(req) : disableRequestLogging
-      if (resolvedDisableRequestLogging === false) {
-        childLogger.info({ req: request }, 'incoming request')
-      }
+      routeEventContext.server[kLogController].incomingRequest(request)
 
       return options.frameworkErrors(new FST_ERR_MAX_PARAM_LENGTH(path), request, reply)
     }
@@ -701,7 +698,7 @@ function fastify (serverOptions) {
           const request = new Request(id, null, req, null, childLogger, routeEventContext)
           const reply = new Reply(res, request, childLogger)
 
-          onBadUrlContext.server[kLogController].incomingRequest(request)
+          routeEventContext.server[kLogController].incomingRequest(request)
 
           return options.frameworkErrors(new FST_ERR_ASYNC_CONSTRAINT(), request, reply)
         }
